4c960c020ad975adaff5278a51db35a2e7fa867c
Francisco Rivera <franmrivera@gmail.com>
2018-05-26 15:04:40 -0300

    tests

--- practico_05/ejercicio_02.py
+++ practico_05/ejercicio_02.py
@@ -36,7 +36,7 @@ class DatosSocio(object):
         Devuelve True si el borrado fue exitoso.
         :rtype: bool
         """
-        return True
+        return False
 
     def alta(self, socio):
         """
@@ -52,7 +52,7 @@ class DatosSocio(object):
         Devuelve True si el borrado fue exitoso.
         :rtype: bool
         """
-        return True
+        return False
 
     def modificacion(self, socio):
         """
@@ -62,3 +62,40 @@ class DatosSocio(object):
         :rtype: Socio
         """
         return socio
+
+
+def pruebas():
+    # alta
+    datos = DatosSocio()
+    socio = datos.alta(Socio(dni=12345678, nombre='Juan', apellido='Perez'))
+    assert socio.id > 0
+
+    # baja
+    assert datos.baja(socio.id) == True
+
+    # buscar
+    socio_2 = datos.alta(Socio(dni=12345679, nombre='Carlos', apellido='Perez'))
+    assert datos.buscar(socio_2.id) == socio_2
+
+    # modificacion
+    socio_3 = datos.alta(Socio(dni=12345680, nombre='Susana', apellido='Gimenez'))
+    socio_3.nombre = 'Moria'
+    socio_3.apellido = 'Casan'
+    socio_3.dni = 13264587
+    datos.modificacion(socio_3)
+    socio_3_modificado = datos.buscar(socio_3.id)
+    assert socio_3_modificado.id == socio_3.id
+    assert socio_3_modificado.nombre == 'Moria'
+    assert socio_3_modificado.apellido == 'Casan'
+    assert socio_3_modificado.dni == 13264587
+
+    # todos
+    assert len(datos.todos()) == 2
+
+    # borrar todos
+    datos.borrar_todos()
+    assert len(datos.todos()) == 0
+
+
+if __name__ == '__main__':
+    pruebas()
