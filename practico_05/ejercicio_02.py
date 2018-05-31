3f88ae4b83cead1bdf988d7681e1b540c885bd86
Francisco Rivera <franmrivera@gmail.com>
2018-05-31 17:23:48 -0300

    TP 05 (#4)
    * tp05
    
    * tests

---
+++ practico_05/ejercicio_02.py
@@ -0,0 +1,101 @@
+# Implementar los metodos de la capa de datos de socios.
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from practico_05.ejercicio_01 import Base, Socio
+
+
+class DatosSocio(object):
+
+    def __init__(self):
+        engine = create_engine('sqlite:///socios.db')
+        Base.metadata.bind = engine
+        db_session = sessionmaker()
+        db_session.bind = engine
+        self.session = db_session()
+
+    def buscar(self, id_socio):
+        """
+        Devuelve la instancia del socio, dado su id.
+        Devuelve None si no encuentra nada.
+        :rtype: Socio
+        """
+        return
+
+    def todos(self):
+        """
+        Devuelve listado de todos los socios en la base de datos.
+        :rtype: list
+        """
+        return []
+
+    def borrar_todos(self):
+        """
+        Borra todos los socios de la base de datos.
+        Devuelve True si el borrado fue exitoso.
+        :rtype: bool
+        """
+        return False
+
+    def alta(self, socio):
+        """
+        Devuelve el Socio luego de darlo de alta.
+        :type socio: Socio
+        :rtype: Socio
+        """
+        return socio
+
+    def baja(self, id_socio):
+        """
+        Borra el socio especificado por el id.
+        Devuelve True si el borrado fue exitoso.
+        :rtype: bool
+        """
+        return False
+
+    def modificacion(self, socio):
+        """
+        Guarda un socio con sus datos modificados.
+        Devuelve el Socio modificado.
+        :type socio: Socio
+        :rtype: Socio
+        """
+        return socio
+
+
+def pruebas():
+    # alta
+    datos = DatosSocio()
+    socio = datos.alta(Socio(dni=12345678, nombre='Juan', apellido='Perez'))
+    assert socio.id > 0
+
+    # baja
+    assert datos.baja(socio.id) == True
+
+    # buscar
+    socio_2 = datos.alta(Socio(dni=12345679, nombre='Carlos', apellido='Perez'))
+    assert datos.buscar(socio_2.id) == socio_2
+
+    # modificacion
+    socio_3 = datos.alta(Socio(dni=12345680, nombre='Susana', apellido='Gimenez'))
+    socio_3.nombre = 'Moria'
+    socio_3.apellido = 'Casan'
+    socio_3.dni = 13264587
+    datos.modificacion(socio_3)
+    socio_3_modificado = datos.buscar(socio_3.id)
+    assert socio_3_modificado.id == socio_3.id
+    assert socio_3_modificado.nombre == 'Moria'
+    assert socio_3_modificado.apellido == 'Casan'
+    assert socio_3_modificado.dni == 13264587
+
+    # todos
+    assert len(datos.todos()) == 2
+
+    # borrar todos
+    datos.borrar_todos()
+    assert len(datos.todos()) == 0
+
+
+if __name__ == '__main__':
+    pruebas()
