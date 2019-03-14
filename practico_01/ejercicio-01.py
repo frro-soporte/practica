34b7ac8e4e6d67edf5c5d6519bd278827b82882b
tomim19 <48568855+tomim19@users.noreply.github.com>
2019-03-14 18:06:38 -0300

    Update ejercicio-01.py

--- practico_01/ejercicio-01.py
+++ practico_01/ejercicio-01.py
@@ -1,5 +1,22 @@
-# Implementar la función suma, que reciba dos números y devuelva su suma.
+#Implementar la función mayor, que reciba tres números y devuelva el mayor de ellos.
 
+a=3
+b=6
+c=2
 
-def suma(a, b):
-    pass
+def mayor(a, b, c):
+    if a > b and a > c:
+        solucion = a
+    elif b > c:
+        solucion = b
+    else:
+        solucion = c
+    return(solucion)
+
+assert mayor(1,2,3) == 3
+assert mayor(3,2,1) == 3
+assert mayor(1,3,2) == 3
+assert mayor(3,3,3) == 3
+assert mayor(1,3,3) == 3
+assert mayor(3,1,3) == 3
+assert mayor(3,3,1) == 3
