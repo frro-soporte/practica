4b899f845e7d9ec2ace468cdd609151c7e92d63a
tomim19 <48568855+tomim19@users.noreply.github.com>
2019-03-16 15:58:03 -0300

    tp1

--- practico_01/ejercicio-07.py
+++ practico_01/ejercicio-07.py
@@ -1,7 +1,13 @@
-# Implementar la función numeros_al_final(), que mueve todos los elementos numéricos
-# de lista al final de esta. Devuelve la lista.
+"""7. Definir una función es_palindromo() que reconoce palíndromos (es decir, palabras
+que tienen el mismo aspecto escritas invertidas), ejemplo: es_palindromo ("radar")
+tendría que devolver True."""
 
-
-# Resolver sin utilizar lista auxiliar
-def numeros_al_final(lista):
-    pass
+nom = "abalaba"
+def es_palindromo(x):
+    palindromo = x[::-1]
+    if palindromo == x:
+        return True
+    else:
+        return False
+print (es_palindromo(nom))
+assert es_palindromo("abalaba") == True
