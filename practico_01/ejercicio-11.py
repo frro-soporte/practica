e10b1cf5f2bb8458d67a7a91fe00bd407332c653
tomim19 <48568855+tomim19@users.noreply.github.com>
2019-03-16 15:59:25 -0300

    tp1

--- practico_01/ejercicio-11.py
+++ practico_01/ejercicio-11.py
@@ -1,6 +1,10 @@
-# Implementar la función es_primo(), que devuelva un booleano en base a
-# si numero es primo o no.
+"""11. Determinar la cantidad de dígitos de un número ingresado."""
 
+valor = 1234
+def con(x):
+    mat = str(x)
+    resultado = len(mat)
+    return resultado
 
-def es_primo(numero):
-    pass
+print(con(valor))
+assert con(1234) == 4
