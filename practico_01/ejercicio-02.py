ae00fdc0c4794217db9986d82394f65a039f5b04
Nahuel <nahuelvila_99@hotmail.com>
2019-03-17 02:51:44 -0300

    Ejercicios: 01, 02 terminados

--- practico_01/ejercicio-02.py
+++ practico_01/ejercicio-02.py
@@ -2,4 +2,17 @@
 
 
 def mayor(a, b, c):
+    if a >=b:
+        if a >=c:
+            return a
+        elif c >=b:
+            return c
+    elif b >= c:
+        return b
     pass
+
+n1 = input('Ingrese 1er numero: ')
+n2 = input('Ingrese 2do numero: ')
+n3 = input('Ingrese 3er numero: ')
+
+print ("el numero mayor es: " + max(n1,n2,n3))
