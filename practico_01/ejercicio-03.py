a365e3edc6748ce23ffd44d6a288f9edc3fe1041
AlejandroDonnola <48600965+AlejandroDonnola@users.noreply.github.com>
2019-03-18 19:14:12 -0300

    Update ejercicio-03.py

--- practico_01/ejercicio-03.py
+++ practico_01/ejercicio-03.py
@@ -3,6 +3,17 @@
 # - Si multiplicar es False: devolver la division entre a y b.
 # - Si multiplicar es False y b es cero: imprimir por consola "Operación no valida".
 
-
 def operacion(a, b, multiplicar):
-    pass
+    if multiplicar is True :
+        return a*b
+    elif multiplicar is False and b != 0:
+        return a/b
+    elif multiplicar is False :
+        return 'Operacion no valida'
+
+
+
+assert operacion(10,5,False) == 2
+assert operacion(10,5,True) == 50
+assert operacion(10,0,False) == 'Operacion no valida'
+
