867ef518131c29af9e072ac622534d9e1d22b5fa
AlejandroDonnola <48600965+AlejandroDonnola@users.noreply.github.com>
2019-03-18 19:53:52 -0300

    Update ejercicio-05.py

--- practico_01/ejercicio-05.py
+++ practico_01/ejercicio-05.py
@@ -3,5 +3,17 @@
 
 
 # Resolver utilizando listas y el operador in.
+
+vocales=['a', 'e', 'i', 'o', 'u']
+
 def es_vocal(letra):
-    pass
+    es=False;
+    for vocal in vocales:
+        if vocal==letra:
+            es= True
+            return es
+        else : return es
+
+
+assert es_vocal('a') == True
+assert es_vocal('x') == False
