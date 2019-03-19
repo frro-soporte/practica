b8cd335fb166a151e2d1795847fe50470bb3b202
AlejandroDonnola <48600965+AlejandroDonnola@users.noreply.github.com>
2019-03-18 21:40:17 -0300

    Update ejercicio-07.py

--- practico_01/ejercicio-07.py
+++ practico_01/ejercicio-07.py
@@ -4,4 +4,18 @@
 
 # Resolver sin utilizar lista auxiliar
 def numeros_al_final(lista):
-    pass
+    i=0
+    for letra in lista:
+        i=i+1
+        if i < len(lista):
+            if isinstance(lista[i], int):
+                lista.append(lista[i])
+                lista.pop(i)
+
+        elif i == len(lista):
+
+                return lista
+
+
+lista = ['a', 3, 'b', 2 , 'c' , 5, 5]
+assert numeros_al_final(lista) == ['a', 'b', 'c', 5, 2, 3, 5]
