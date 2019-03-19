2a2ab9a6476afd7bfde3a08a3e6a8da51de49b60
junnchy <juanichacho@gmail.com>
2019-03-19 16:06:49 -0300

    ejercicos 2 y 4

--- practico_01/ejercicio-02.py
+++ practico_01/ejercicio-02.py
@@ -1,5 +1,19 @@
 # Implementar la función mayor, que reciba tres números y devuelva el mayor de ellos.
 
-
 def mayor(a, b, c):
-    pass
+    if a > b:
+        if a > c:
+            return a
+        else:
+            return c
+    else:
+        if b > c:
+            return b
+        else:
+            return c
+
+print(mayor(2,2,2))
+print(mayor(3,1,1))
+print(mayor(1,4,1))
+print(mayor(1,1,5))
+
--- practico_01/ejercicio-04.py
+++ practico_01/ejercicio-04.py
@@ -1,6 +1,12 @@
-# Implementar la función conversor, que ingrese desde la consola grados Celsius
-# y los devuelva transformados a Fahrenheit.
+# Escribir una funcin que tome un caracter y devuelva True si es Vocal, de lo contrario devuelve false
 
 
-def conversor(grados):
-    pass
+
+def esvocal(let):
+    if let=="a" or let=="e" or let=="i" or let=="o" or let=="u":
+        return True
+    else:
+        return False
+
+print(esvocal("v"))
+print(esvocal("a"))
