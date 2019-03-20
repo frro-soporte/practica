5cd8d64f78b08be61c9439ff1a99abcaadbe3759
Milton Sotto Diaz <48601521+miltonsd@users.noreply.github.com>
2019-03-20 17:03:55 -0300

    Update ejercicio-02.py

--- practico_01/ejercicio-02.py
+++ practico_01/ejercicio-02.py
@@ -1,6 +1,14 @@
 # Implementar la función mayor, que reciba tres números y devuelva el mayor de ellos.
 
-
 def mayor(a, b, c):
-    if 
-    pass
+    g=0
+    if (a>b and a>c):
+        g=a
+    else:
+        if (b>c):
+            g=b
+        else:
+            g=c
+
+    return g
+assert(mayor(600,500,400)==600)
