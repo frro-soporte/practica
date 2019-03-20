2609c5816b3dea05e7f06e07c44e0bf41b4a849e
orcorsetti <48458591+orcorsetti@users.noreply.github.com>
2019-03-20 11:33:21 -0300

    Delete ejercicio-03.py

--- practico_01/ejercicio-03.py
+++
@@ -1,8 +0,0 @@
-# Implementar la función operacion, donde:
-# - Si multiplicar es True: devolver la multiplicación entre a y b.
-# - Si multiplicar es False: devolver la division entre a y b.
-# - Si multiplicar es False y b es cero: imprimir por consola "Operación no valida".
-
-
-def operacion(a, b, multiplicar):
-    pass
