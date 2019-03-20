2ca0c2754594c589d1139a5dc1bfb2f319b47335
orcorsetti <48458591+orcorsetti@users.noreply.github.com>
2019-03-20 11:34:13 -0300

    Delete ejercicio-08.py

--- practico_01/ejercicio-08.py
+++
@@ -1,8 +0,0 @@
-# Implementar la función mitad(), que devuelve la mitad de palabra.
-# Si la longitud es impar, redondear hacia arriba.
-
-
-# hola -> ho
-# verde -> ver
-def mitad(palabra):
-    pass
