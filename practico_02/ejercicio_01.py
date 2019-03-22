92f9027897423393bd315889fa0dc015e0a450fb
Milton Sotto Diaz <48601521+miltonsd@users.noreply.github.com>
2019-03-22 16:39:46 -0300

    Update ejercicio_01.py

--- practico_02/ejercicio_01.py
+++ practico_02/ejercicio_01.py
@@ -19,3 +19,22 @@ class Rectangulo:
 
       def function(self):
            print "This is a message inside the class."
+
+        
+        
+        
+        class Rectangulo:
+
+    def __init__(self, base, altura):
+        self.x = base
+        self.y = altura
+
+    def  area(self):
+        
+        return self.x*self.y
+
+
+
+x = Rectangulo(10,10)
+
+assert Rectangulo.Area() == 100
