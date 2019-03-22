56234224cfb84395f2f5ef644498993f8b01cdec
nicolasf96 <39437815+nicolasf96@users.noreply.github.com>
2019-03-22 16:18:19 -0300

    Update ejercicio-05.py

--- practico_01/ejercicio-05.py
+++ practico_01/ejercicio-05.py
@@ -3,5 +3,17 @@
 
 
 # Resolver utilizando listas y el operador in.
-def es_vocal(letra):
-    pass
+
+def es_vocal(c):
+    vocales = ['a', 'e', 'i' , 'o' ,'u']
+    c = c.lower()
+    if c in vocales:
+        return True
+    else:
+        return False
+
+
+assert es_vocal('t') == False
+assert es_vocal('a') == True
+assert es_vocal('E') == True
+
