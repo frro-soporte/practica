03f497fd4457c4b67486c4bcb488fd9ff2c5280e
Milton Sotto Diaz <48601521+miltonsd@users.noreply.github.com>
2019-03-22 16:09:02 -0300

    Update ejercicio-10.py

--- practico_01/ejercicio-10.py
+++ practico_01/ejercicio-10.py
@@ -4,9 +4,25 @@
 
 # se debe implementar utilizando bucles anidados.
 def superposicion_loop(lista_1, lista_2):
-    pass
+    z = False
+    for i in lista_1:
+        for j in lista_2:
+            if i == j:
+                z = True
+                break
+        if z == True:
+            break
+    return z
 
 
 # se debe implementar utilizando conjuntos (sets).
 def superposicion_set(lista_1, lista_2):
-    pass
+    z = lista_1 & lista_2
+    if z:
+        return True
+    else:
+        return False
+
+
+assert superposicion_loop("adas","aefg") == True
+assert superposicion_set({1, 2, 3, 4, 5},{1, 6, 7, 8, 0}) == True
