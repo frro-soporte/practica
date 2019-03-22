043d3307dddc13bd133d390e77086f7a616854a8
Milton Sotto Diaz <48601521+miltonsd@users.noreply.github.com>
2019-03-22 16:23:58 -0300

    Update ejercicio-11.py

--- practico_01/ejercicio-11.py
+++ practico_01/ejercicio-11.py
@@ -2,5 +2,21 @@
 # si numero es primo o no.
 
 
-def es_primo(numero):
-    pass
+def es_primo(num):
+    z=False
+    if num > 1:
+     for i in range(2,num):
+         if (num % i) == 0:
+             z=False
+             return z
+             break
+         else:
+          z=True
+          return z
+    else :
+        return z
+
+
+assert es_primo(4) == False
+assert es_primo(7) == True
+
