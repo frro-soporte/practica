763e6e787fec64c63dc57d996791e0772619f99c
nicolasf96 <39437815+nicolasf96@users.noreply.github.com>
2019-03-22 15:36:06 -0300

    Update ejercicio-02.py

--- practico_01/ejercicio-02.py
+++ practico_01/ejercicio-02.py
@@ -2,4 +2,14 @@
 
 
 def mayor(a, b, c):
-    pass
+   if(a > b and a > c):
+       return(a)
+   else:
+       if(b > a and b > c):
+           return(b)
+       else:
+           return(c)
+
+
+assert mayor(10, 15, 20) == 20
+assert mayor(1, 2, 0) == 2
