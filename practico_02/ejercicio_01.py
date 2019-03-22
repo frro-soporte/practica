def5c3abf6b37e8333979db5071e4b44b2082116
Milton Sotto Diaz <48601521+miltonsd@users.noreply.github.com>
2019-03-22 16:35:50 -0300

    Update ejercicio_01.py

--- practico_02/ejercicio_01.py
+++ practico_02/ejercicio_01.py
@@ -1,10 +1,21 @@
 # Implementar la clase Rectangulo que contiene una base y una altura, y el método area.
 
 
-class Rectangulo:
-
+class Rectangulo: 
+    global x, y
     def __init__(self, base, altura):
-        pass
+        x = base
+        y = altura
 
-    def area(self):
-        pass
+    def  area(self):
+        
+        return x*y 
+    
+    
+    
+    
+    class MyClass:
+      variable = "blah"
+
+      def function(self):
+           print "This is a message inside the class."
