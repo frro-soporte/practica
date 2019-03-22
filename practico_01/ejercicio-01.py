a0cde4a94a9ea76071c92e621cd224509db51188
nicolasf96 <39437815+nicolasf96@users.noreply.github.com>
2019-03-22 15:25:05 -0300

    Update ejercicio-01.py

--- practico_01/ejercicio-01.py
+++ practico_01/ejercicio-01.py
@@ -1,5 +1,16 @@
 # 1. Implementar una función max() que tome como argumento dos números y devuelva el mayor de ellos. 
 
 
-def maximo(a, b):
-    pass
+def max(a, b):
+   if a < b:
+       return(b)
+   return(a)
+
+
+print("Ingresar numero 1: ")
+num1 = input()
+print("Ingresar numero 2: ")
+num2 = input()
+
+
+print("El número max es: ",max(num1,num2))
