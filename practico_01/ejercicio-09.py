c8bf5199697caeadf3cae6ac4a52f663f069266a
AlejandroDonnola <48600965+AlejandroDonnola@users.noreply.github.com>
2019-03-22 15:42:06 -0300

    Update ejercicio-09.py

--- practico_01/ejercicio-09.py
+++ practico_01/ejercicio-09.py
@@ -4,5 +4,13 @@
 
 # Ejemplos: arenera, radar, ojo, oso, salas.
 # Resolver sin utilizar loops (for/while), sino con slicing.
+
+
 def es_palindromo(palabra):
-    pass
+    z = False
+    x= len(palabra)
+    if palabra[0:x] == palabra [::-1]:
+        z = True
+    return  z
+
+assert es_palindromo("radar") == True
