b0e150171b56db47bf05686d55bc51bd6ad504b9
nicomessina <nikoxrc@gmail.com>
2019-03-23 16:30:27 -0300

    fin practico 1; falta revisar ej7 que de 2 formas distintas ocurre lo mismo

--- practico_01/ejercicio-08.py
+++ practico_01/ejercicio-08.py
@@ -1,8 +1,18 @@
 # Implementar la función mitad(), que devuelve la mitad de palabra.
 # Si la longitud es impar, redondear hacia arriba.
+import math
 
 
 # hola -> ho
 # verde -> ver
 def mitad(palabra):
-    pass
+    largo = len(palabra)
+    if(largo/2!=0):
+        largo = math.ceil(largo/2)
+    else: largo=largo/2
+    mitad= palabra[0:largo]
+    return mitad
+
+
+assert (mitad('pala')) == 'pa';
+assert (mitad('palabra')) == 'pala';
--- practico_01/ejercicio-11.py
+++ practico_01/ejercicio-11.py
@@ -3,4 +3,44 @@
 
 
 def es_primo(numero):
-    pass
+    if (numero>=1):
+        resultado=False
+        if(numero==1 or numero==2):
+            return True
+
+        else:
+            if((numero % 2)!=0):
+                if(numero%3 !=0):
+                    if (numero % 5 != 0):
+                        if (numero % 7 != 0):
+
+                            if (numero % 11 != 0):
+                                return True
+                            else:
+                                if(numero==11):
+                                    return True
+                        else:
+                            if(numero==7):
+                                return True
+
+                    else:
+                        if (numero == 5):
+                            return True
+                else:
+                    if(numero==3):
+                        return True
+
+            return resultado
+
+
+
+    else:
+        print('Ingresaste un numero menor que 1')
+
+
+
+assert es_primo(3) == True;
+assert es_primo(113) == True;
+assert es_primo(4) == False;
+
+
