a96f84462dba2bf41da8ef3a76afe9326064951e
Nano <ginocarlucci@hotmail.com>
2019-03-26 20:25:23 -0300

    Agrego el ejercicio 07

--- practico_01/ejercicio-07.py
+++ practico_01/ejercicio-07.py
@@ -4,4 +4,16 @@
 
 # Resolver sin utilizar lista auxiliar
 def numeros_al_final(lista):
+    i=0
+    for i in range(len(lista)):
+        if (lista[i].isdigit() == True):
+            aux = lista[i]
+            lista.pop(i)
+            lista.append(aux)
     pass
+
+lista = ["1","a","3","b","5"]
+numeros_al_final(lista)
+print(lista)
+assert numeros_al_final(lista) == ["a", "b", "1", "3", "5"]
+
