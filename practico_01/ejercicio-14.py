f0f469ec365a1bb7733bed0162b5ad021b91a90a
leonsch1997 <48569065+leonsch1997@users.noreply.github.com>
2019-03-27 14:39:56 -0300

    Update ejercicio-14.py

--- practico_01/ejercicio-14.py
+++ practico_01/ejercicio-14.py
@@ -6,31 +6,40 @@
 
 
 def busca_salida(m):
-    for i in range (0,len(m)):        #funcion donde se fija en la matriz si hay un 
-                                      # False a la derecha, abajo, a la izquierda o arriba
-                                      # si tiene que volver por donde ya pasó, vuelve.
+    inicio=m[1][1]
+    final=m[2][6]
+    for i in range (0,len(m)):          # funcion donde se fija en la matriz si hay un
+                                        # False a la derecha, abajo, a la izquierda o arriba
+                                        # si tiene que volver por donde ya pasó, vuelve.
         for j in range(0,len(m[i])):
-            if (not m[i][j+1] or m[i][j+1]=="x"):
-                m[i][j+1]="x"
-                return m
-            else:
-                if (not m[i+1][j]or m[i+1][j]=="x"):
+            if(m[i]!=len(m) or m[j]!=len(m[i])):
+                if (not m[i][j+1] or m[i][j+1]=="x" and m[i][j+1]!=final):
+                    m[i][j+1]="x"
+                    for k in range(4):
+                        print(m[k])
+
+                elif(not m[i+1][j]or m[i+1][j]=="x" and m[i+1][j]!=final):
                     m[i+1][j]="x"
-                    return m
-                else:
-                    if (not m[i][j-1]or m[i][j-1]=="x"):
-                        m[i][j-1]="x"
-                        return m
-                    else:
-                        if (not m[i-1][j]or m[i-1][j]=="x"):
-                            m[i-1][j]="x"
-                            return m
-    busca_salida(m)   #no tengo muy claro como hacerlo recursivo sin que produzca error de indice
+                    for k in range(4):
+                        print(m[k])
+
+                elif(not m[i][j-1]or m[i][j-1]=="x" and m[i][j-1]!=final):
+                    m[i][j-1]="x"
+                    for k in range(4):
+                        print(m[k])
+
+                elif(not m[i-1][j]or m[i-1][j]=="x" and m[i-1][j]!=final):
+                    m[i-1][j]="x"
+                    for k in range(4):
+                        print(m[k])
+    return m
+                            #no tengo muy claro como hacerlo recursivo sin que produzca error de indice
 
 
 mat=([["x", True, True, True, False, False, True],  #creacion del laberinto con obstaculos
      [False, False, False, True, True, False, True], #"x" es el objeto que recorre el laberinto
      [True, False, True, True, False, False, False],
      [True, False, False, False, False, True, True]])
-
-print(busca_salida(mat))
+for i in range(4):
+    print(mat[i])
+nuevamat=busca_salida(mat)
