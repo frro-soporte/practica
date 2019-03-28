1508c87cab35dff1c5932a0720694b8648fd4864
Nahuel <nahuelvila_99@hotmail.com>
2019-03-28 18:30:06 -0300

    Terminado ejercicio 01 y 02

--- practico_02/ejercicio_01.py
+++ practico_02/ejercicio_01.py
@@ -4,7 +4,18 @@
 class Rectangulo:
 
     def __init__(self, base, altura):
-        pass
+        self.base = base
+        self.altura = altura
 
     def area(self):
-        pass
+        return self.base * self.altura
+
+
+base = int(input("Ingrese la base"))
+altura = int(input("Ingrese la altura"))
+
+rectangulo1 = Rectangulo(base, altura)
+
+print("La base es: ", rectangulo1.base)
+print("La altura es: ", rectangulo1.altura)
+print("El area es: ",rectangulo1.area())
--- practico_02/ejercicio_02.py
+++ practico_02/ejercicio_02.py
@@ -1,13 +1,27 @@
 # Implementar la clase Circulo que contiene un radio, y sus métodos area y perimetro.
-
+# area = pi * rr
+import math
 
 class Circulo:
 
     def __init__(self, radio):
-        pass
+        self.radio = radio
+
 
     def area(self):
-        pass
+        return math.pi * math.pow(self.radio,2)
 
     def perimetro(self):
-        pass
+        return  math.pi * 2 * self.radio
+
+radio = int(input("Ingrese el radio: "))
+
+circulo1 = Circulo(radio)
+
+print("")
+print("El radio es: ", circulo1.radio)
+print("")
+print("El area es: ", circulo1.area())
+print("")
+print("El perimetr es: ", circulo1.perimetro())
+
