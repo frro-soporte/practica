860b4e5253ba2afc565fb1159dffcf9ea2a31d50
nicolasf96 <nico.fierro1@gmail.com>
2019-03-28 11:52:41 -0300

    agrego ejercicio 11

--- practico_01/ejercicio-11.py
+++ practico_01/ejercicio-11.py
@@ -3,4 +3,19 @@
 
 
 def es_primo(numero):
-    pass
+    a=0
+    for i in range(1,numero+1):
+        if((numero%i) == 0):
+            a=a+1
+
+    if(a == 2):
+        bol=True
+        return bol
+    else:
+        bol=False
+        return bol
+
+num = 7
+assert es_primo(num) == True
+assert es_primo(269)==True
+assert es_primo(4) == False
