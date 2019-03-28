14a6e9c67459d31d1efe0c1b157c239f6b62fb62
nicolasf96 <nico.fierro1@gmail.com>
2019-03-28 15:42:40 -0300

    agrego ejercicio 10

--- practico_01/ejercicio-10.py
+++ practico_01/ejercicio-10.py
@@ -4,22 +4,44 @@
 
 # se debe implementar utilizando bucles anidados.
 
-def superposicion_loop(lista_1, lista_2):
-    for i in range(lista_1):
-        for x in range(lista_2):
-            if(lista_1[i] == lista_2[x]):
-                bol = True
-                return(bol)
-    pass
+def superposicion_x(lista_1, lista_2):
+    a = 0
+    for i in range(len(lista_1)):
+        for j in range(len(lista_2)):
+            if(lista_1[i]==lista_2[j]):
+                a=a+1
+    if (a>=1):
+        return True
+    else:
+        return False
+
+
+lista_1 = ["1","2", "3", "4"]
+lista_2 = ["5", "6", "7", "2"]
+
+assert superposicion_x(lista_1, lista_2) == True
 
 
 # se debe implementar utilizando conjuntos (sets).
-#def superposicion_set(lista_1, lista_2):
- #   pass
+# def superposicion_set(lista_1, lista_2):
+#   pass
 
-bol = False
-lista_1 = ["a", "2", "b", "8"]
-lista_2 = ["v", "c", "6", "a"]
-#assert superposicion_loop(lista_1, lista_2) == False
-superposicion_loop(lista_1, lista_2)
-print(bol)
+
+def superposicion_set(lista_3, lista_4):
+    a = 0
+    if (lista_3 & lista_4 != set()):
+        a = a + 1
+    if (a >= 1):
+        return True
+    else:
+        return False
+
+
+lista_3 = set([1, 2, 3, 4])
+lista_4 = set([5, 6, 4, 3])
+
+# assert superposicion_set(lista_1, lista_2) == False
+
+assert superposicion_set(lista_3, lista_4) == True
+
+
