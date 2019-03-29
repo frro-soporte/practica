a771a029b769f43d4783c436a7c314b182196e03
gonzalormonja <48839385+gonzalormonja@users.noreply.github.com>
2019-03-29 18:04:41 -0300

    Update ejercicio_05.py

--- practico_02/ejercicio_05.py
+++ practico_02/ejercicio_05.py
@@ -2,7 +2,32 @@
 # y devuelva un diccionario con las carreras como keys, y la cantidad de estudiantes en cada una de ellas como values.
 
 from practico_02.ejercicio_04 import Estudiante
-
-
 def organizar_estudiantes(estudiantes):
-    pass
+    dic = {}
+    for e in estudiantes:
+        if e.carrera in dic:
+            dic[e.carrera] = int(dic[e.carrera])+1
+        else:
+            dic[e.carrera] = 1
+
+    return dic
+
+est = []
+e = Estudiante("isi",2015,10,5)
+est.append(e)
+e = Estudiante("iq",2015,10,5)
+est.append(e)
+e = Estudiante("isi",2015,10,5)
+est.append(e)
+e = Estudiante("im",2015,10,5)
+est.append(e)
+e = Estudiante("isi",2015,10,5)
+est.append(e)
+e = Estudiante("iq",2015,10,5)
+est.append(e)
+e = Estudiante("ie",2015,10,5)
+est.append(e)
+e = Estudiante("ii",2015,10,5)
+est.append(e)
+
+assert organizar_estudiantes(est) == {'isi': 3, 'iq': 2, 'im': 1, 'ie': 1, 'ii': 1}
