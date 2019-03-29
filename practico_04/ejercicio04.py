77e0df31e277e9d3540c9d87972eb34cc2cb4334
Mario Castagnino <31213260+mac3333@users.noreply.github.com>
2019-03-29 16:23:36 -0300

    Create ejercicio04.py

---
+++ practico_04/ejercicio04.py
@@ -0,0 +1,5 @@
+## 4. Ejercicio al Formulario del Ejercicio 3 ,  agrege  los siguientes botones 1- un  botón  Alta 
+## que inicia otra venta donde puedo ingresar una ciudad y su código postal .
+## 2 – un botón Baja que borra del listad de ciudades la ciudad que esta selecionada en Treeview .
+## 3 – un botón Modificar . Todos los cambios se deben ver reflejados en la lista que se muestra . 
+
