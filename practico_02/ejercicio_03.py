5ad44e0a60e467e65b24ea2f43ea54c52e926f4a
catriel <catrielcaruso@gmail.com>
2019-04-01 10:24:11 -0300

    Cambios

--- practico_02/ejercicio_03.py
+++ practico_02/ejercicio_03.py
@@ -9,19 +9,54 @@
 # - es_mayor_edad(): indica si es mayor de edad, devuelve un booleano.
 # - print_data(): imprime por pantalla toda la información del objeto.
 # - generar_dni(): genera un número aleatorio de 8 cifras y lo guarda dentro del atributo dni.
-
+from random import randint
 
 class Persona:
 
     def __init__(self, nombre, edad, sexo, peso, altura):
-        pass
+
+        self.nombre = nombre
+        self.edad = edad
+        self.sexo = sexo
+        self.peso = peso
+        self.altura = altura
+
+    pass
 
     def es_mayor_edad(self):
-        pass
 
-    # llamarlo desde __init__
+        if (self.edad < 18):
+            return False
+        else:
+
+            return True
+
+    pass
+
     def generar_dni(self):
-        pass
+
+        numeros = []
+        for x in range(8):
+            a = randint(0, 9)
+            numeros.append(str(a))
+
+        dni = ''
+        for x in range(8):
+            dni = dni + numeros[x]
+
+        return(dni)
+
+    pass
 
     def print_data(self):
-        pass
+
+        return self.nombre, self.edad, self.sexo, self.peso, self.altura
+
+    pass
+
+
+per = Persona("catriel", 20, 80, 1.85, "masculino")
+
+print("Datos de la persona: ",per.print_data())
+print("Es mayor de edad: ",per.es_mayor_edad())
+print("El dni es: ",per.generar_dni())
