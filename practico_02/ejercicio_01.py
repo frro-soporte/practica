a0a73568eeb1b8a3d34e2e24b6d15c3d8979091c
catriel <catrielcaruso@gmail.com>
2019-04-01 08:22:10 -0300

    Cambios

--- practico_02/ejercicio_01.py
+++ practico_02/ejercicio_01.py
@@ -4,7 +4,17 @@
 class Rectangulo:
 
     def __init__(self, base, altura):
+
+        self.base=base
+        self.altura=base
+
         pass
 
     def area(self):
+        return self.base*self.altura
+
         pass
+
+re=Rectangulo(1.2,3)
+
+print(re.area())
