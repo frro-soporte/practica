ed264617ee2b9530805baca4c2226b48c1424e07
AlejandroDonnola <48600965+AlejandroDonnola@users.noreply.github.com>
2019-04-04 00:26:01 -0300

    Update ejercicio_05.py

--- practico_02/ejercicio_05.py
+++ practico_02/ejercicio_05.py
@@ -4,5 +4,27 @@
 from practico_02.ejercicio_04 import Estudiante
 
 
-def organizar_estudiantes(estudiantes):
-    pass
+def organizar_estudiantes(e):
+    dcarrera = {}
+    x=0
+    for i in e :
+        if not dcarrera.get(i.carr) :
+         x=0
+         dcarrera.update({i.carr : x+1})
+        else:
+          x=dcarrera.get(i.carr)
+          dcarrera.update({i.carr : x+1})
+    print(dcarrera)
+    return dcarrera
+
+e = []
+x = Estudiante("Ingeniería en Sistemas", 2018, 20, 10, "Fulanito", 19, 'H', 64.3, 1.72)
+y = Estudiante("Ingeniería en Sistemas", 2018, 20, 10, "Cosme", 19, 'H', 64.3, 1.72)
+z = Estudiante("Ingeniería Quimica", 2018, 20, 10, "Fulanito", 19, 'H', 64.3, 1.72)
+w = Estudiante("Ingeniería Quimica", 2018, 20, 10, "Cosme", 19, 'H', 64.3, 1.72)
+e.append(x)
+e.append(y)
+e.append(z)
+e.append(w)
+
+assert organizar_estudiantes(e) == {'Ingeniería en Sistemas': 2, 'Ingeniería Quimica': 2}
