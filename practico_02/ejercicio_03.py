6ba0caca8733d19d8828e224bc18880592bb3799
Nahuel <nahuelvila_99@hotmail.com>
2019-04-09 22:19:23 -0300

    ejercicios 3 y 4 terminados

--- practico_02/ejercicio_03.py
+++ practico_02/ejercicio_03.py
@@ -10,18 +10,57 @@
 # - print_data(): imprime por pantalla toda la información del objeto.
 # - generar_dni(): genera un número aleatorio de 8 cifras y lo guarda dentro del atributo dni.
 
+from random import randint
 
 class Persona:
 
-    def __init__(self, nombre, edad, sexo, peso, altura):
-        pass
+    def __init__(self, nombre, edad, sexo, peso, altura, dni):
+        self.nombre = nombre
+        self.edad = edad
+        self.sexo = sexo
+        self.peso = peso
+        self.altura = altura
+        self.dni = dni
 
     def es_mayor_edad(self):
-        pass
+
+        if self.edad >= 18:
+            return True
+        else:
+            return False
 
     # llamarlo desde __init__
     def generar_dni(self):
-        pass
+        for i in range (8):
+            self.dni += str(randint(0,9))
+        return self.dni
 
     def print_data(self):
-        pass
+        print("Nombre: ", self.nombre)
+        print("Edad: ", self.edad)
+        print("Sexo: ", self.sexo)
+        print("Peso: ", self.peso)
+        print("Altura: ", self.altura)
+        print("DNI: ", self.dni)
+
+def app():
+
+    alumno1 = Persona
+
+    alumno1.nombre = 'Nahuel'
+    alumno1.edad = 26
+    alumno1.sexo = 'H'
+    alumno1.peso = 90
+    alumno1.altura = 1.75
+    alumno1.dni = ''
+
+    alumno1.es_mayor_edad(alumno1)
+    alumno1.generar_dni(alumno1)
+    alumno1.print_data(alumno1)
+
+
+
+
+if __name__ == '__main__':
+
+    app()
--- practico_02/ejercicio_04.py
+++ practico_02/ejercicio_04.py
@@ -8,15 +8,48 @@
 # - avance(): indica que porcentaje de la carrera tiene aprobada.
 # - edad_ingreso(): indica que edad tenia al ingresar a la carrera (basándose en el año actual).
 
+from practico_02.ejercicio_03 import Persona
+from datetime import datetime
 
-class Estudiante:
+class Estudiante(Persona):
 
     def __init__(self, carrera, anio, cantidad_materias, cantidad_aprobadas):
-        pass
+        self.carrera = carrera
+        self.anio = anio
+        self.cantidad_materias = cantidad_materias
+        self.cantidad_aprobadas = cantidad_aprobadas
+
+        Persona.__init__(self, nombre, edad, sexo, peso, altura, dni)
+
+
 
     def avance(self):
-        pass
+       print("El avance de la carrera es un: ", (self.cantidad_aprobadas/self.cantidad_materias)*100,'%')
 
     # implementar usando modulo datetime
     def edad_ingreso(self):
-        pass
+        now = datetime.now()
+        dif_anio = now.year - self.anio
+
+        print("Edad de ingreso a la carrera: ", self.edad - dif_anio)
+
+def app():
+
+    estudiante1 = Estudiante
+
+    estudiante1.carrera = 'Ingenieria en sistemas de informacion'
+    estudiante1.anio = 2013
+    estudiante1.cantidad_materias = 40
+    estudiante1.cantidad_aprobadas = 15
+    estudiante1.edad = 26
+
+    estudiante1.edad_ingreso(estudiante1)
+    estudiante1.avance(estudiante1)
+
+
+
+
+
+if __name__ == '__main__':
+
+    app()
