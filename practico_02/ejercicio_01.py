54a11bc743c30e2609760f12c7a29f56e0d7b881
nicomessina <nikoxrc@gmail.com>
2019-04-12 18:33:57 -0300

    Ejercicio 1, 2 y 4

--- practico_02/ejercicio_01.py
+++ practico_02/ejercicio_01.py
@@ -3,8 +3,18 @@
 
 class Rectangulo:
 
+
     def __init__(self, base, altura):
-        pass
+        self.rec_base = base
+        self.rec_altura = altura
 
     def area(self):
-        pass
+        area = self.rec_altura*self.rec_base
+        print('Area del rectangulo: ' + str(area) + ' mts')
+        return area
+
+base= 8
+altura= 10
+rectangulo = Rectangulo(base,altura)
+
+assert(rectangulo.area())==80
--- practico_02/ejercicio_02.py
+++ practico_02/ejercicio_02.py
@@ -1,13 +1,20 @@
 # Implementar la clase Circulo que contiene un radio, y sus métodos area y perimetro.
-
+import math
 
 class Circulo:
-
     def __init__(self, radio):
-        pass
+        self.radio = radio
 
     def area(self):
-        pass
+        area = round(math.pi * pow(self.radio,2),2)
+        return area
 
     def perimetro(self):
-        pass
+        perimetro = round(2*math.pi*self.radio,2)
+        return perimetro
+
+
+circulo =Circulo(2)
+
+assert(circulo.area())== 12.57;
+assert(circulo.perimetro())==12.57;
