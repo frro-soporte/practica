6d37a17a4c381d1fd03d227856854ba703125a0c
AlejandroDonnola <48600965+AlejandroDonnola@users.noreply.github.com>
2019-04-19 15:11:57 -0300

    Update ejercicio_01.py

--- practico_03/ejercicio_01.py
+++ practico_03/ejercicio_01.py
@@ -8,15 +8,30 @@
 # Implementar la funcion borrar_tabla, que borra la tabla creada anteriormente.
 
 
+import pymysql
+
+# Open db
+db = pymysql.connect(host='localhost', user='root', password='852456ale', port=3306, db='Python')
+# Crear cursor
+cursor = db.cursor()
+
 def crear_tabla():
+    cursor.execute("CREATE TABLE Persona(IdPersona int key auto_increment,"
+               " Nombre char(30), "
+               "FechaNacimiento date,"
+               " DNI int,"
+               " Altura int)")
+    print("Tabla creada")
+    db.close()
     pass
 
 
 def borrar_tabla():
+    cursor.execute("DROP TABLE Persona")
+    print("Tabla borrada")
+    db.close()
     pass
 
-
-# no modificar
 def reset_tabla(func):
     def func_wrapper():
         crear_tabla()
