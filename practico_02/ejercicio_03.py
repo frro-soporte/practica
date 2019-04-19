7b7de3ac3bb7ff06cbc56adbdd6edc12d14181ef
giulitruant <giulitruant_26@hotmail.com>
2019-04-19 15:57:33 -0300

    fix clase hereda Persona

--- practico_02/ejercicio_03.py
+++ practico_02/ejercicio_03.py
@@ -10,18 +10,43 @@
 # - print_data(): imprime por pantalla toda la información del objeto.
 # - generar_dni(): genera un número aleatorio de 8 cifras y lo guarda dentro del atributo dni.
 
-
 class Persona:
 
     def __init__(self, nombre, edad, sexo, peso, altura):
-        pass
+        self.nombre = nombre
+        self.edad = edad
+        self.sexo = sexo
+        self.peso = peso
+        self.altura = altura
+        self.generar_dni()
 
     def es_mayor_edad(self):
-        pass
+        if(self.edad >= 18 ):
+            return True
+        else:
+            return False
 
     # llamarlo desde __init__
     def generar_dni(self):
-        pass
+        import random
+        for x in range(10):
+            self.dni = random.randint(1,90)
 
     def print_data(self):
-        pass
+        resultado = "El nombre es: {}, es mayor de edad: {}, es de sexo: {}, tiene un peso de {}," \
+                    " tiene una altura de {}".format(self.nombre, self.es_mayor_edad(), self.sexo, self.peso, self.altura)
+        print(resultado)
+
+#try:
+#nombre = input('Ingresar nombre')
+#edad = int(input('Ingresar edad'))
+#sexo = input('Ingresar sexo M\F')
+#peso = float(input('Ingresar peso'))
+#altura = float(input('Ingresar altura'))
+p = Persona('marcos', 18, 'M', 76.5, 1.82)
+print(p.print_data())
+
+#except ValueError:
+ #   print('Ingreso datos erroneos, intente nuevamente')
+
+
--- practico_02/ejercicio_04.py
+++ practico_02/ejercicio_04.py
@@ -9,16 +9,47 @@
 # - edad_ingreso(): indica que edad tenia al ingresar a la carrera (basándose en el año actual).
 
 
-import datetime
-from ejercicio_03 import Persona
-
-
 # Probar si funciona bien importando la clase de otro ejercicio.
+from datetime import datetime
+
+#import ejercicio_03
+
+class Persona:
+
+    def __init__(self, nombre, edad, sexo, peso, altura):
+        self.nombre = nombre
+        self.edad = edad
+        self.sexo = sexo
+        self.peso = peso
+        self.altura = altura
+        self.generar_dni()
+
+    def es_mayor_edad(self):
+        if(self.edad >= 18 ):
+            return True
+        else:
+            return False
+
+    # llamarlo desde __init__
+    def generar_dni(self):
+        import random
+        for x in range(10):
+            self.dni = random.randint(1,90)
+
+    def print_data(self):
+        resultado = "El nombre es: {}, es mayor de edad: {}, es de sexo: {}, tiene un peso de {}," \
+                    " tiene una altura de {}".format(self.nombre, self.es_mayor_edad(), self.sexo, self.peso, self.altura)
+        print(resultado)
 
 class Estudiante(Persona):
 
     def __init__(self, nombre, edad, sexo, peso, altura, carrera, año, cant_materias, cant_aprobadas):
-        Persona.__init__(self,nombre,edad,sexo,peso,altura)
+        self.nombre = nombre
+        self.edad = edad
+        self.sexo = peso
+        self.altura = altura
+
+        #self.Persona.__init__(self,nombre,edad,sexo,peso,altura)
         self.carrera = carrera
         self.año = año
         self.cant_materias = cant_materias
@@ -33,6 +64,7 @@ class Estudiante(Persona):
     # implementar usando modulo datetime
     def edad_ingreso(self):
         ing = self.edad - (datetime.datetime.now().year - self.año)
+#from ejercicio_03 import Persona
         return ing
 
 es = Estudiante("Pepe", 24 , "M" ,78 ,1.75 ,"INGENIERIA EN SISTEMAS", 2013 , 25 ,20)
