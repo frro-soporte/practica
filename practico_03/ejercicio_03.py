7ec2c1764cca464fc0becd8ae3801181f38ab73d
Milton Sotto Diaz <48601521+miltonsd@users.noreply.github.com>
2019-04-25 22:04:15 -0300

    Update ejercicio_03.py

--- practico_03/ejercicio_03.py
+++ practico_03/ejercicio_03.py
@@ -2,6 +2,7 @@
 # Devuelve un booleano en base a si encontro el registro y lo borro o no.
 
 import datetime
+import pymysql
 db = pymysql.connect(host='localhost', user='root', password='852456', port=3306, db='Python')
 cursor = db.cursor()
 from practico_03.ejercicio_01 import reset_tabla
