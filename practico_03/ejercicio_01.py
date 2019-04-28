2c19690c311150d4d9ee62c9b2af7cdbeff565aa
giulitruant <giulitruant_26@hotmail.com>
2019-04-28 19:44:03 -0300

    ejercicio 1,2 realizados

--- practico_03/ejercicio_01.py
+++ practico_03/ejercicio_01.py
@@ -7,19 +7,46 @@
 
 # Implementar la funcion borrar_tabla, que borra la tabla creada anteriormente.
 
+import mysql.connector
+
+def conexion():
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="",
+    database="Pruebas-Python"
+    )
+    return mydb
+
 
 def crear_tabla():
-    pass
-
+    conn = conexion()
+    mycursor = conn.cursor()
+    sql = (
+        """
+        CREATE TABLE Persona (IdPersona int(11) NOT NULL auto_increment primary key,
+        Nombre char(30),
+        FechaNacimiemto date,
+        DNI int(8),
+        Altura int(3))"""
+    )
+    mycursor.execute(sql)
+    print('creacion de tabla con exito')
 
 def borrar_tabla():
-    pass
+    conn = conexion()
+    mycursor = conn.cursor()
+    sql = ("drop table Persona")
+    mycursor.execute(sql)
+    print('eliminacion de tabla con exito')
 
 
-# no modificar
+#ultima funcion no modificar
 def reset_tabla(func):
     def func_wrapper():
         crear_tabla()
         func()
         borrar_tabla()
     return func_wrapper
+
+crear_tabla()
--- practico_03/ejercicio_02.py
+++ practico_03/ejercicio_02.py
@@ -3,14 +3,38 @@
 
 import datetime
 
-from practico_03.ejercicio_01 import reset_tabla
+#from Practico_03.ejercicio_01 import reset_tabla
 
+import mysql.connector
+
+def conexion():
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="",
+    database="Pruebas-Python"
+    )
+    return mydb
 
 def agregar_persona(nombre, nacimiento, dni, altura):
-    return 0
+    conn = conexion()
+    mycursor = conn.cursor()
+    sql = "insert into Persona (Nombre, FechaNacimiemto, DNI, Altura) values (%s, %s, %s, %s);"
+    datos =(str(nombre), nacimiento, dni, altura)
+    mycursor.execute(sql, datos)
+    conn.commit()
+    sql = "SELECT LAST_INSERT_ID();"
+
+    mycursor.execute(sql)
+    data= mycursor.fetchall()
+    for pers in data:
+        id = pers[0]
+
+    print("El id es {0}, el nombre {1}, la fecha de nacimiento {2}, el nro dni {3} y la altura es {4}".format(id, nombre, nacimiento, dni, altura))
+    return id
 
 
-@reset_tabla
+#@reset_tabla
 def pruebas():
     id_juan = agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180)
     id_marcela = agregar_persona('marcela gonzalez', datetime.datetime(1980, 1, 25), 12164492, 195)
