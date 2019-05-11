6d0d32969667d2cf44a3723b7ddcc2e2e39fc85a
AlejandroDonnola <48600965+AlejandroDonnola@users.noreply.github.com>
2019-05-11 00:22:02 -0300

    Update ejercicio_03.py

--- practico_03A/ejercicio_03.py
+++ practico_03A/ejercicio_03.py
@@ -1,17 +1,33 @@
 # Implementar la funcion borrar_persona, que elimina un registro en la tabla Persona.
 # Devuelve un booleano en base a si encontro el registro y lo borro o no.
 
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from Practica01Alchemy import Persona
+from Practica02Alchemy import agregar_persona
 import datetime
 
-from practico_03.ejercicio_01 import reset_tabla
-from practico_03.ejercicio_02 import agregar_persona
-
+Base = declarative_base()
+engine = create_engine('mysql://root:852456ale@localhost:3306/python')
+Base.metadata.bind = engine
+DBSession = sessionmaker()
+DBSession.bind = engine
+session = DBSession()
 
 def borrar_persona(id_persona):
-    return False
+    obj=session.query(Persona).filter(Persona.IdPersona==id_persona).first()
+    if obj is None:
+        return False
+    else:
+        x=session.delete(obj)
+        session.commit()
+        return True
 
 
-@reset_tabla
+
+
 def pruebas():
     assert borrar_persona(agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180))
     assert borrar_persona(12345) is False
