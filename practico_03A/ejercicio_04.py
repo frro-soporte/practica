8e1aec3ec355ab183f8c02fc3c9bf0bdf604de27
AlejandroDonnola <48600965+AlejandroDonnola@users.noreply.github.com>
2019-05-11 00:48:16 -0300

    Update ejercicio_04.py

--- practico_03A/ejercicio_04.py
+++ practico_03A/ejercicio_04.py
@@ -2,21 +2,38 @@
 # El return es una tupla que contiene sus campos: id, nombre, nacimiento, dni y altura.
 # Si no encuentra ningun registro, devuelve False.
 
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from Practica01Alchemy import Persona,reset_tabla
+from Practica02Alchemy import agregar_persona
 import datetime
 
-from practico_03.ejercicio_01 import reset_tabla
-from practico_03.ejercicio_02 import agregar_persona
+Base = declarative_base()
+engine = create_engine('mysql://root:852456ale@localhost:3306/python')
+Base.metadata.bind = engine
+DBSession = sessionmaker()
+DBSession.bind = engine
+session = DBSession()
 
 
 def buscar_persona(id_persona):
-    return False
+    obj=session.query(Persona).filter(Persona.IdPersona==id_persona).first()
+
+    session.commit()
+    if obj is None:
+     return False
+    else:
+     return (obj.IdPersona,obj.Nombre,obj.FechaNacimiento,obj.DNI,obj.Altura)
 
 
 @reset_tabla
+
 def pruebas():
     juan = buscar_persona(agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180))
-    assert juan == (1, 'juan perez', datetime.datetime(1988, 5, 15), 32165498, 180)
+    assert juan == (1, 'juan perez', datetime.date(1988, 5, 15), 32165498, 180)
     assert buscar_persona(12345) is False
+
 
 if __name__ == '__main__':
     pruebas()
