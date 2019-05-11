561bba35ac4b1e8082ff3fee869a97fa0526c2fb
AlejandroDonnola <48600965+AlejandroDonnola@users.noreply.github.com>
2019-05-10 23:31:19 -0300

    Create ejercicio_02.py

---
+++ practico_03A/ejercicio_02.py
@@ -0,0 +1,41 @@
+# Implementar la funcion agregar_persona, que inserte un registro en la tabla Persona
+# y devuelva los datos ingresados el id del nuevo registro.
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from Practica01Alchemy import Persona
+import datetime
+
+Base = declarative_base()
+engine = create_engine('mysql://root:852456ale@localhost:3306/python')
+Base.metadata.bind = engine
+DBSession = sessionmaker()
+DBSession.bind = engine
+session = DBSession()
+
+
+def agregar_persona(nombre, nacimiento, dni, altura):
+    per = Persona(nombre,nacimiento,dni,altura)
+    session.add(per)
+    session.commit()
+    session.refresh(per)
+    id=per.IdPersona
+    return id
+
+def pruebas():
+    id_juan = agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180)
+    id_marcela = agregar_persona('marcela gonzalez', datetime.datetime(1980, 1, 25), 12164492, 195)
+    print(id_juan,id_marcela)
+    assert id_juan > 0
+    assert id_marcela > id_juan
+
+pruebas()
+
+def reset_tabla(func):
+    def func_wrapper():
+        crear_tabla()
+        func()
+        borrar_tabla()
+    return func_wrapper
