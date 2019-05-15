2feb1798bd67e669e9f123d169dab17bd0107216
AlejandroDonnola <48600965+AlejandroDonnola@users.noreply.github.com>
2019-05-15 16:49:04 -0300

    Create ejercicio_06.py

---
+++ practico_03A/ejercicio_06.py
@@ -0,0 +1,51 @@
+# Implementar la funcion crear_tabla_peso, que cree una tabla PersonaPeso con:
+# - IdPersona: Int() (Clave Foranea Persona)
+# - Fecha: Date()
+# - Peso: Int()
+
+# Implementar la funcion borrar_tabla, que borra la tabla creada anteriormente.
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, ForeignKey, Integer, String , Date,ForeignKey
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker,relationship
+from ejercicio_01.py import borrar_tabla,crear_tabla,Persona
+
+
+
+import datetime
+
+Base = declarative_base()
+engine = create_engine('mysql://root:852456ale@localhost:3306/python')
+Base.metadata.bind = engine
+DBSession = sessionmaker()
+DBSession.bind = engine
+session = DBSession()
+
+class PersonaPeso:
+    __tablename__ = 'personapeso'
+    persona = relationship(Persona)
+    IdPersona = Column(Integer, ForeignKey(Persona.IdPersona))
+    FechaNacimiento = Column(Date, nullable=False)
+    Peso= Column(Integer, nullable=False)
+
+
+
+    def __init__(self,nacimiento, peso):
+        self.Peso = peso
+        self.FechaNacimiento=nacimiento
+
+
+def crear_tabla_peso():
+   Base.metadata.create_all(engine)
+def borrar_tabla_peso():
+   PersonaPeso.__table__.drop(engine)
+   
+   
+def reset_tabla(func):
+    def func_wrapper():
+        crear_tabla()
+        crear_tabla_peso()
+        func()
+        borrar_tabla_peso()
+        borrar_tabla()
+    return func_wrapper
