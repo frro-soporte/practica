afd7c9518eea62372ed4c1d775e829a6da3fad75
AlejandroDonnola <48600965+AlejandroDonnola@users.noreply.github.com>
2019-05-15 11:26:40 -0300

    Update ejercicio_04.py

--- practico_03A/ejercicio_04.py
+++ practico_03A/ejercicio_04.py
@@ -5,8 +5,8 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from Practica01Alchemy import Persona,reset_tabla
-from Practica02Alchemy import agregar_persona
+from ejercicio_01 import Persona,reset_tabla
+from ejercicio_02 import agregar_persona
 import datetime
 
 Base = declarative_base()
