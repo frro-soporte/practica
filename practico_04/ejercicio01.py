714e1417c55d04b14c2a48f539baa1ee863ea9e0
nicomessina <nikoxrc@gmail.com>
2019-05-18 13:13:01 -0300

    fin ejercicio 1 - Practico 4

--- practico_04/ejercicio01.py
+++ practico_04/ejercicio01.py
@@ -1,6 +1,6 @@
 ## 1 Ejercicio Hacer un formulario tkinter que es una calculadora, tiene 2 entry para ingresar los valores V1 y V2.
 ## Y 4 botones de operaciones para las operaciones respectivas + , - , * , / ,
-## al cliquearlos muestre el resultado de aplicar el operador respectivo en los V1 y V2 . 
+## al cliquearlos muestre el resultado de aplicar el operador respectivo en los V1 y V2 .
 
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
@@ -9,43 +9,102 @@
 # compatible el interfaz Tkinter con los programas basados
 # en versiones anteriores a la 8.5, con las más recientes.
 
-from tkinter import *    # Carga módulo tk (widgets estándar)
-from tkinter import ttk  # Carga ttk (para widgets nuevos 8.5+)
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 
-# Define la ventana principal de la aplicación
+from tkinter import *
+ventana = Tk()
+ventana.title("Calculadora")
+ventana.geometry("300x150")
 
-raiz = Tk()
+valor_1 = StringVar()
+valor_2 = StringVar()
+resultado = StringVar()
+label1 =Label(ventana,  text ="1er Valor").place(x=10, y=10)
+#label1.grid(row =1, column =0)
 
-# Define las dimensiones de la ventana, que se ubicará en
-# el centro de la pantalla. Si se omite esta línea la
-# ventana se adaptará a los widgets que se coloquen en
-# ella.
+num1_txtbx =Entry(ventana, textvariable=valor_1, width=5).place(x=100, y=10)
+# num1_txtbx.grid(row =1, column =1)
 
-raiz.geometry('300x200') # anchura x altura
+label2 =Label(ventana, text ="2do Valor").place(x=10, y=50)
 
-# Asigna un color de fondo a la ventana. Si se omite
-# esta línea el fondo será gris
+num2_txtbx =Entry(ventana, textvariable=valor_2,width=5).place(x=100, y=50)
 
-raiz.configure(bg = 'beige')
+def suma():
+    
+    if (valor_1.get() and valor_2.get() != ""):
+        try:
+            num1 =float(valor_1.get())
+            num2 =float(valor_2.get())
+            answer = 'Resultado:', num1 + num2
+            resultado.set(answer)
+            valor_1.set('')            
+            valor_2.set('')
 
-# Asigna un título a la ventana
+        except:
+            resultado.set("Entrada invalida, vuelva a intentarlo")
+    else:
+        resultado.set("Se deben llenar todos los campos")
 
-raiz.title('Aplicación')
 
-# Define un botón en la parte inferior de la ventana
-# que cuando sea presionado hará que termine el programa.
-# El primer parámetro indica el nombre de la ventana 'raiz'
-# donde se ubicará el botón
-ttk.Button(raiz, text='Sumar', command=quit).pack(side=RIGHT)
-ttk.Button(raiz, text='Salir', command=quit).pack(side=BOTTOM)
+def resta():
+    if (valor_1.get() and valor_2.get() != ""):
+        try:
+            num1 = float(valor_1.get())
+            num2 = float(valor_2.get())
+            answer = 'Resultado:', num1 - num2
+            resultado.set(answer)
+            valor_1.set('')            
+            valor_2.set('')
 
-# Después de definir la ventana principal y un widget botón
-# la siguiente línea hará que cuando se ejecute el programa
-# construya y muestre la ventana, quedando a la espera de
-# que alguna persona interactúe con ella.
+        except:
+            resultado.set("Entrada invalida, vuelva a intentarlo")
+    else:
+        resultado.set("Se deben llenar todos los campos")
 
-# Si la persona presiona sobre el botón Cerrar 'X', o bien,
-# sobre el botón 'Salir' el programa llegará a su fin.
 
-raiz.mainloop()
+def multiplicacion():
+    if (valor_1.get() and valor_2.get() != ""):
+        try:
+            num1 = float(valor_1.get())
+            num2 = float(valor_2.get())
+            answer = 'Resultado:', num1 * num2
+            resultado.set(answer)
+            valor_1.set('')            
+            valor_2.set('')
+
+        except:
+            resultado.set("Entrada invalida, vuelva a intentarlo")
+    else:
+        resultado.set("Se deben llenar todos los campos")
+
+
+def division():
+    if (valor_1.get() and valor_2.get() != ""):
+
+        try:
+            num1 = float(valor_1.get())
+            num2 = float(valor_2.get())
+            answer = 'Resultado:', num1 / num2
+            resultado.set(answer)
+            valor_1.set('')            
+            valor_2.set('')
+
+        except:
+            resultado.set("Entrada invalida, vuelva a intentarlo")
+    else:
+        resultado.set("Se deben llenar todos los campos")
+
+ancho_boton = 1
+alto_boton = 1
+
+suma =Button(ventana, text="+",width=ancho_boton, height=alto_boton, command= suma).place(x=170, y=10)
+resta=Button(ventana, text="-", width=ancho_boton, height=alto_boton,command= resta).place(x=220, y=10)
+multi=Button(ventana, text="x",width=ancho_boton, height=alto_boton, command= multiplicacion).place(x=170, y=45)
+division=Button(ventana, text="%",width=ancho_boton, height=alto_boton, command= division).place(x=220, y=45)
+
+# salida = Entry(ventana,textvariable=resultado, justify='center').place(x=50,y=100)
+salida2 = Label(ventana, textvariable=resultado, justify='center').place(x=50,y=100)
+
+ventana.mainloop()
