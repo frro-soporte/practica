833b36ccdf0eb325b906905e440fcadea0b70267
Pablo Uriel Alvarez <38137567+9urialv@users.noreply.github.com>
2019-05-31 16:53:28 -0300

    Add files via upload

--- practico_01/ejercicio-01.py
+++ practico_01/ejercicio-01.py
@@ -1,9 +1,13 @@
-# 1. Implementar una función max() que tome como argumento dos números y devuelva el mayor de ellos. 
+"""Practica1 Ejercicio 1 Alumno: Pablo Uriel Alvarez Grupo 29"""
 
+"""Defino la funcion Max()"""
+def max(num1,num2):
+    if num1>=num2:
+        return num1
+    else:
+        return num2
 
-def maximo(a, b):
-    pass
+#Utilizo el asset para verificar
+assert max(10,9) == 10
+assert max(10,12) == 12
 
-# si no falla es porque esta bien
-assert maximo(10, 5) == 10
-assert maximo(9, 18) == 18
