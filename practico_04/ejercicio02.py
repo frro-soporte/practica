6572d6d4f9fb939c95d1462661198551e32cb217
Gabriel Capitanelli <gabrielcapitanelli@hotmail.com>
2019-06-15 01:05:56 -0300

    Practica 4 Terminada

--- practico_04/ejercicio02.py
+++ practico_04/ejercicio02.py
@@ -1,3 +1,61 @@
 ## 2 Ejercicio Hacer un formulario en Tkinter una calculadora que tenga 1 entry y 12 botones para los dígitos 0 al 9
 ## y las operaciones + - / * = , que al apretar cada botón vaya agregando al valor que muestra en el entry el carácter 
 ## que le corresponde ( como se ve imagen ) y cuando se aprieta en = pone el resultado de evaluar la cadena entrada . 
+
+from tkinter import *
+from math import *
+
+raiz=Tk()
+raiz.title("Calculadora")
+raiz.geometry("250x400")
+raiz.configure(bg="beige")
+color_boton=("gray77")
+
+def btnClik(num):
+    global operador
+    operador=operador+str(num)
+    entrada_txt.set(operador)
+
+def clear():
+    global operador
+    txt = entrada_txt.get()[:-1]
+    if entrada_txt.get() == 'Error':
+        txt = ''
+    entrada_txt.set(txt)
+    operador=txt
+
+def operacion():
+    global operador
+    try:
+        resultado=str(eval(operador))
+    except:
+        clear()
+        resultado=("Error")
+    entrada_txt.set(resultado)
+    operador = resultado
+
+ancho=3
+alto=1
+entrada_txt=StringVar()
+operador=""
+clear()
+
+Boton7=Button(raiz,text="7",bg=color_boton,width=ancho,height=alto,command=lambda:btnClik(7)).place(x=15,y=150)
+Boton8=Button(raiz,text="8",bg=color_boton,width=ancho,height=alto,command=lambda:btnClik(8)).place(x=65,y=150)
+Boton9=Button(raiz,text="9",bg=color_boton,width=ancho,height=alto,command=lambda:btnClik(9)).place(x=115,y=150)
+BotonSuma=Button(raiz,text="+",bg=color_boton,width=ancho,height=alto,command=lambda:btnClik("+")).place(x=165,y=150)
+Boton4=Button(raiz,text="4",bg=color_boton,width=ancho,height=alto,command=lambda:btnClik(4)).place(x=15,y=200)
+Boton5=Button(raiz,text="5",bg=color_boton,width=ancho,height=alto,command=lambda:btnClik(5)).place(x=65,y=200)
+Boton6=Button(raiz,text="6",bg=color_boton,width=ancho,height=alto,command=lambda:btnClik(6)).place(x=115,y=200)
+BotonResta=Button(raiz,text="-",bg=color_boton,width=ancho,height=alto,command=lambda:btnClik("-")).place(x=165,y=200)
+Boton1=Button(raiz,text="1",bg=color_boton,width=ancho,height=alto,command=lambda:btnClik(1)).place(x=15,y=250)
+Boton2=Button(raiz,text="2",bg=color_boton,width=ancho,height=alto,command=lambda:btnClik(2)).place(x=65,y=250)
+Boton3=Button(raiz,text="3",bg=color_boton,width=ancho,height=alto,command=lambda:btnClik(3)).place(x=115,y=250)
+BotonDiv=Button(raiz,text="/",bg=color_boton,width=ancho,height=alto,command=lambda:btnClik("/")).place(x=165,y=250)
+Boton0=Button(raiz,text="0",bg=color_boton,width=ancho,height=alto,command=lambda:btnClik(0)).place(x=15,y=300)
+BotonResul=Button(raiz,text="=",bg=color_boton,width=10,height=alto,command=operacion).place(x=65,y=300)
+BotonMulti=Button(raiz,text="x",bg=color_boton,width=ancho,height=alto,command=lambda:btnClik("*")).place(x=165,y=300)
+
+Salida=Entry(raiz,font=('arial',20,'bold'),width=13,textvariable=entrada_txt,justify="left").place(x=10,y=60)
+
+raiz.mainloop()
--- practico_04/ejercicio04.py
+++ practico_04/ejercicio04.py
@@ -3,3 +3,91 @@
 ## 2 – un botón Baja que borra del listad de ciudades la ciudad que esta selecionada en Treeview .
 ## 3 – un botón Modificar . Todos los cambios se deben ver reflejados en la lista que se muestra . 
 
+from tkinter import *
+from tkinter import ttk
+
+
+class Aplicacion():
+
+        def __init__(self):
+                self.raiz = Tk()
+                self.raiz.title("Ciudades Argentinas")
+                self.tree = ttk.Treeview(self.raiz)
+                self.raiz.config(background="light blue")
+#Variable
+                self.ciudad=StringVar()
+                self.codpos=IntVar()
+                self.i = 0
+#Columnas
+                self.tree["columns"]=("1","2")
+                self.tree.column("#0", width=100)
+                self.tree.column("1", width=100 )
+                self.tree.column("2", width=100)
+                self.tree.heading("#0", text="Id")
+                self.tree.heading("1", text="Ciudad")
+                self.tree.heading("2", text="Código Postal")
+#Insert
+                self.boton0 = ttk.Button(self.raiz, text="Agregar Ciudad", command=self.grilla)
+                self.boton1 = ttk.Button(self.raiz, text="Eliminar Ciudad", command=self.Baja)
+                self.boton2 = ttk.Button(self.raiz, text="Modificar Ciudad", command=self.grilla2)
+                self.boton0.grid(row=1, column=5)
+                self.boton1.grid(row=2, column=5)
+                self.boton2.grid(row=3, column=5)
+#Loop
+                self.tree.grid()
+                self.raiz.mainloop()
+#Alta
+        def Agrega(self):
+                self.tree.insert('', 'end', text=str(self.i), values=(self.ciudad.get(), self.codpos.get()),iid=self.i)
+                self.i = self.i + 1
+                self.ventana.destroy()
+
+        def grilla(self):
+                txt = ''
+                self.ciudad.set(txt)
+                self.codpos.set(txt)
+                self.ventana = Toplevel()
+                self.ventana.title("Nueva ciudad")
+                self.label0 = ttk.Label(self.ventana, text="Ciudad: ")
+                self.label1 = ttk.Label(self.ventana, text="Codigo postal:")
+                self.entry0 = ttk.Entry(self.ventana, textvariable=self.ciudad, width=30)
+                self.entry1 = ttk.Entry(self.ventana, textvariable=self.codpos, width=30)
+                self.boton0 = ttk.Button(self.ventana, text="Agregar", command=self.Agrega)
+                self.label0.grid(row=0, column=0)
+                self.label1.grid(row=1, column=0)
+                self.entry0.grid(row=0, column=1)
+                self.entry1.grid(row=1, column=1)
+                self.boton0.grid(row=2, column=2)
+
+#Baja
+        def Baja(self):
+                selected_item = self.tree.selection()[0]
+                self.tree.delete(selected_item)
+#Modificacion
+        def Modificar(self):
+                x = self.tree.selection()[0]
+                for item in x:
+                        self.tree.item(item, values=(self.ciudad.get(), self.codpos.get()))
+                self.ventana.destroy()
+
+        def grilla2(self):
+                self.ventana = Toplevel()
+                self.ventana.title("Modificar ciudad")
+                self.label0 = ttk.Label(self.ventana, text="Ciudad: ")
+                self.label1 = ttk.Label(self.ventana, text="Codigo postal:")
+                self.entry0 = ttk.Entry(self.ventana, textvariable=self.ciudad, width=30)
+                self.entry1 = ttk.Entry(self.ventana, textvariable=self.codpos, width=30)
+                self.boton0 = ttk.Button(self.ventana, text="Modificar", command=self.Modificar)
+                self.label0.grid(row=0, column=0)
+                self.label1.grid(row=1, column=0)
+                self.entry0.grid(row=0, column=1)
+                self.entry1.grid(row=1, column=1)
+                self.boton0.grid(row=2, column=2)
+
+
+def main():
+    mi_app = Aplicacion()
+    return mi_app
+
+if __name__ == '__main__':
+    main()
