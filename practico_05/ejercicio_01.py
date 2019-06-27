e5211738ff021045b35d152637e5c0df76d54681
Nahuel <nahuelvila_99@hotmail.com>
2019-06-27 16:41:39 -0300

    Ejercicios 01 y 02 terminados

--- practico_05/ejercicio_01.py
+++ practico_05/ejercicio_01.py
@@ -5,14 +5,22 @@
 # - apellido: string (longitud 250)
 
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 
 class Socio(Base):
     __tablename__ = 'socios'
+    id_socio = Column(Integer, primary_key=True,unique=True, autoincrement=True)
+    dni = Column(Integer, unique=True)
+    nombre = Column(String(250))
+    apellido = Column(String(250))
 
-    # id = Column(...)
-    # dni = Column(...)
-    # nombre = Column(...)
-    # apellido = Column(...)
+
+engine = create_engine('sqlite:///c://Users//Nahuel//Desktop//db_tp5.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker()
+DBSession.bind = engine
+session = DBSession()
--- practico_05/ejercicio_02.py
+++ practico_05/ejercicio_02.py
@@ -9,85 +9,80 @@ from practico_05.ejercicio_01 import Base, Socio
 class DatosSocio(object):
 
     def __init__(self):
-        engine = create_engine('sqlite:///socios.db')
+        engine = create_engine('sqlite:///socios.db', echo=True)
+        Base.metadata.drop_all(engine)
         Base.metadata.bind = engine
+        Base.metadata.create_all(engine)
         db_session = sessionmaker()
         db_session.bind = engine
         self.session = db_session()
 
     def buscar(self, id_socio):
-        """
-        Devuelve la instancia del socio, dado su id.
-        Devuelve None si no encuentra nada.
-        :rtype: Socio
-        """
-        return
+        try:
+            return self.session.query(Socio).filter_by(id_socio=id_socio).first()
+        except:
+            return None
 
     def buscar_dni(self, dni_socio):
-        """
-        Devuelve la instancia del socio, dado su dni.
-        Devuelve None si no encuentra nada.
-        :rtype: Socio
-        """
-        return
+        try:
+            return self.session.query(Socio).filter_by(dni=dni_socio).first()
+        except:
+            return None
 
     def todos(self):
-        """
-        Devuelve listado de todos los socios en la base de datos.
-        :rtype: list
-        """
-        return []
+        try:
+            return self.session.query(Socio).all()
+        except:
+            return None
 
     def borrar_todos(self):
-        """
-        Borra todos los socios de la base de datos.
-        Devuelve True si el borrado fue exitoso.
-        :rtype: bool
-        """
-        return False
+        try:
+            self.session.query(Socio).delete()
+            return True
+        except:
+            return False
 
     def alta(self, socio):
-        """
-        Devuelve el Socio luego de darlo de alta.
-        :type socio: Socio
-        :rtype: Socio
-        """
+        self.session.add(socio)
+        self.session.commit()
         return socio
 
     def baja(self, id_socio):
-        """
-        Borra el socio especificado por el id.
-        Devuelve True si el borrado fue exitoso.
-        :rtype: bool
-        """
-        return False
+        socio = self.buscar(id_socio)
+        if socio is None:
+            return False
+        else:
+            try:
+                self.session.delete(socio)
+                self.session.commit()
+                return True
+            except:
+                return False
 
     def modificacion(self, socio):
-        """
-        Guarda un socio con sus datos modificados.
-        Devuelve el Socio modificado.
-        :type socio: Socio
-        :rtype: Socio
-        """
-        return socio
+        socio_busqueda = self.buscar(socio.id_socio)
+        socio_busqueda.dni = socio.dni
+        socio_busqueda.nombre = socio.nombre
+        socio_busqueda.apellido = socio.apellido
+        self.session.commit()
+        return socio_busqueda
 
 
 def pruebas():
     # alta
     datos = DatosSocio()
     socio = datos.alta(Socio(dni=12345678, nombre='Juan', apellido='Perez'))
-    assert socio.id > 0
+    assert socio.id_socio > 0
 
     # baja
-    assert datos.baja(socio.id) == True
+    assert datos.baja(socio.id_socio) == True
 
     # buscar
     socio_2 = datos.alta(Socio(dni=12345679, nombre='Carlos', apellido='Perez'))
-    assert datos.buscar(socio_2.id) == socio_2
+    assert datos.buscar(socio_2.id_socio) == socio_2
 
     # buscar dni
-    socio_2 = datos.alta(Socio(dni=12345679, nombre='Carlos', apellido='Perez'))
-    assert datos.buscar(socio_2.dni) == socio_2
+    assert datos.buscar_dni(socio_2.dni) == socio_2
 
     # modificacion
     socio_3 = datos.alta(Socio(dni=12345680, nombre='Susana', apellido='Gimenez'))
@@ -95,8 +90,8 @@ def pruebas():
     socio_3.apellido = 'Casan'
     socio_3.dni = 13264587
     datos.modificacion(socio_3)
-    socio_3_modificado = datos.buscar(socio_3.id)
-    assert socio_3_modificado.id == socio_3.id
+    socio_3_modificado = datos.buscar(socio_3.id_socio)
+    assert socio_3_modificado.id_socio == socio_3.id_socio
     assert socio_3_modificado.nombre == 'Moria'
     assert socio_3_modificado.apellido == 'Casan'
     assert socio_3_modificado.dni == 13264587
