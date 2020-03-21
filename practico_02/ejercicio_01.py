59a638fb44cecf7c2ebddec4269338c9ce530479
oberruti <octavio@profitwell.com>
2020-03-20 21:18:42 -0300

    Practica 1 a 4 hechas

--- practico_02/ejercicio_01.py
+++ practico_02/ejercicio_01.py
@@ -4,7 +4,11 @@
 class Rectangulo:
 
     def __init__(self, base, altura):
-        pass
+        self.base = base
+        self.altura = altura
 
     def area(self):
-        pass
+        return self.base * self.altura
+
+
+assert Rectangulo(8, 4).area() == 32
--- practico_02/ejercicio_04.py
+++ practico_02/ejercicio_04.py
@@ -7,16 +7,32 @@
 # Métodos:
 # - avance(): indica que porcentaje de la carrera tiene aprobada.
 # - edad_ingreso(): indica que edad tenia al ingresar a la carrera (basándose en el año actual).
+from practico_02.ejercicio_03 import Persona
+from datetime import datetime
 
 
-class Estudiante:
+class Estudiante(Persona):
 
-    def __init__(self, carrera, anio, cantidad_materias, cantidad_aprobadas):
-        pass
+    def __init__(self, nombre, edad, sexo, peso, altura, carrera, anio, cantidad_materias, cantidad_aprobadas):
+        self.carrera = carrera
+        self.anio = anio
+        self.cantidad_materias = cantidad_materias
+        self.cantidad_aprobadas = cantidad_aprobadas
+
+        Persona.__init__(self, nombre, edad, sexo, peso, altura)
 
     def avance(self):
-        pass
+        avance = self.cantidad_aprobadas / self.cantidad_materias
+        porcentaje = avance * 100
+        return round(porcentaje, 2)
 
     # implementar usando modulo datetime
     def edad_ingreso(self):
-        pass
+        anio_ingreso = datetime.strptime(self.anio, "%Y").year
+        anio__actual = datetime.utcnow().year
+        anios_cursados = anio__actual - anio_ingreso
+
+        return self.edad - anios_cursados
+
+
+assert Estudiante("pedro", 24, "H", 98, 18, "Sistemas", "2018", 18, 6).edad_ingreso() == 22
