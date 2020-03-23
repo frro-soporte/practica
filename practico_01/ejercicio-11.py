12aef95bace85191b01f59cdc45b81572e4b6768
eche <etxeagus@gmail.com>
2020-03-23 16:11:17 -0300

    Tp 1

--- practico_01/ejercicio-11.py
+++ practico_01/ejercicio-11.py
@@ -3,4 +3,22 @@
 
 
 def es_primo(numero):
-    pass
+    b = True
+    for i in range (2,numero):
+        if numero % i == 0:
+            b = False
+    return b
+
+def main():
+    if es_primo(19):
+        print("Es primo")
+    else:
+        print("No es primo")
+
+    if es_primo(10):
+        print("Es primo")
+    else:
+        print("No es primo")
+
+
+main()
