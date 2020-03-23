1df75a58e2aa2dd1ffc259602cb5d2b25c4f8bf1
oberruti <octavio@profitwell.com>
2020-03-23 16:48:02 -0300

    ejercicio 11 corregido

--- practico_01/ejercicio-11.py
+++ practico_01/ejercicio-11.py
@@ -3,10 +3,18 @@
 
 
 def es_primo(numero):
-    if numero == 1:
-        return True
-    else:
-        for i in range(2, numero):
-            if numero % i == 0:
-                return False
-        return True
+    if numero == 1 or numero == 0:
+        return False
+
+    for i in range(2, numero):
+        if numero % i == 0:
+            return False
+    return True
+
+
+assert not es_primo(0)
+assert not es_primo(1)
+assert es_primo(2)
+assert es_primo(3)
+assert not es_primo(4)
+assert es_primo(5)
