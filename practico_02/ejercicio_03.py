afc09cd95f48b423d38b6d07dfa34745820dc11b
Damian <49252212+DamianBarzola@users.noreply.github.com>
2020-03-23 18:04:01 -0300

    practico-02

--- practico_02/ejercicio_03.py
+++ practico_02/ejercicio_03.py
@@ -9,19 +9,41 @@
 # - es_mayor_edad(): indica si es mayor de edad, devuelve un booleano.
 # - print_data(): imprime por pantalla toda la información del objeto.
 # - generar_dni(): genera un número aleatorio de 8 cifras y lo guarda dentro del atributo dni.
+import random
 
 
 class Persona:
 
     def __init__(self, nombre, edad, sexo, peso, altura):
-        pass
+        self.nombre=nombre
+        self.edad=edad
+        self.sexo=sexo
+        self.peso=peso
+        self.altura=altura
+        self.dni = self.generar_dni()
 
     def es_mayor_edad(self):
-        pass
+        if self.edad>=18:
+            return True
+        else:
+            return False
+
 
     # llamarlo desde __init__
     def generar_dni(self):
-        pass
+        dni = random.randint(1000000, 50000000)
+        return dni
 
     def print_data(self):
-        pass
+        print("DNI:", self.dni)
+        print("Nombre:", self.nombre)
+        print("Edad:", self.edad)
+        print("Sexo:", self.sexo)
+        print("Peso:", self.peso,"Kg")
+        print("Altura:", self.altura,"m")
+
+
+a=Persona("Damian",21,"H",75,1.78)
+print("Es mayor de edad?", a.es_mayor_edad())
+a.print_data()
+
--- practico_02/ejercicio_04.py
+++ practico_02/ejercicio_04.py
@@ -7,16 +7,28 @@
 # Métodos:
 # - avance(): indica que porcentaje de la carrera tiene aprobada.
 # - edad_ingreso(): indica que edad tenia al ingresar a la carrera (basándose en el año actual).
+from datetime import datetime
+from practico_02.ejercicio_03 import Persona
 
 
-class Estudiante:
+class Estudiante(Persona):
 
-    def __init__(self, carrera, anio, cantidad_materias, cantidad_aprobadas):
-        pass
+    def __init__(self,  nombre, edad, sexo, peso, altura, carrera, anio, cantidad_materias, cantidad_aprobadas):
+        Persona.__init__(self, nombre, edad, sexo, peso, altura)
+        self.carrera = carrera
+        self.anio = anio
+        self.cantidad_materias = cantidad_materias
+        self.cantidad_aprobadas = cantidad_aprobadas
+
 
     def avance(self):
-        pass
+        print("Promedio de la Materia Aprobada: ", round(self.cantidad_aprobadas * 100 / self.cantidad_materias,2))
 
     # implementar usando modulo datetime
     def edad_ingreso(self):
-        pass
+        print("Edad al ingresar a la facu: ", self.edad - (datetime.now().year - self.anio))
+
+
+e = Estudiante("Damian",21,"H",75,1.78,'ISI', 2016, 36, 22)
+e.avance()
+e.edad_ingreso()
