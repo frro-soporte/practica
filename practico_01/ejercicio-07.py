7def4217dfba370acdc0a0431018de585b07811e
joaquin8123 <joaquin8123@gmail.com>
2020-03-24 22:21:32 -0300

    Update ejercicio-07.py

--- practico_01/ejercicio-07.py
+++ practico_01/ejercicio-07.py
@@ -1,7 +1,16 @@
 # Implementar la función numeros_al_final(), que mueve todos los elementos numéricos
 # de lista al final de esta. Devuelve la lista.
 
-
 # Resolver sin utilizar lista auxiliar
 def numeros_al_final(lista):
+    i=0
+    for i in range(len(lista)):
+        if (lista[i].isdigit() == True):
+            aux = lista[i]
+            lista.pop(i)
+            lista.append(aux)
     pass
+
+lista = ["1","a","3","b","5","c","v","2"]
+numeros_al_final(lista)
+print(lista)
