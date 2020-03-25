bbb2ac2415896b5ab7760a7dd23e31db27492e96
joaquin8123 <joaquin8123@gmail.com>
2020-03-24 23:07:52 -0300

    Update ejercicio-11.py

--- practico_01/ejercicio-11.py
+++ practico_01/ejercicio-11.py
@@ -1,6 +1,16 @@
 # Implementar la función es_primo(), que devuelva un booleano en base a
 # si numero es primo o no.
 
-
 def es_primo(numero):
+    n = 0
+    for i in range (1,numero+1):
+        if ((numero % i) == 0):
+            n= n+1
+    if (n == 2):
+        return True
+    else:
+        return False
     pass
+
+assert es_primo(8) == False
+assert es_primo(11) == True
