52a53fb5f5c2c49f0ecba77bb22e3ad49c2bd9d5
joaquin8123 <joaquin8123@gmail.com>
2020-03-26 16:19:54 -0300

    Update ejercicio_03.py

--- practico_02/ejercicio_03.py
+++ practico_02/ejercicio_03.py
@@ -9,19 +9,29 @@
 # - es_mayor_edad(): indica si es mayor de edad, devuelve un booleano.
 # - print_data(): imprime por pantalla toda la información del objeto.
 # - generar_dni(): genera un número aleatorio de 8 cifras y lo guarda dentro del atributo dni.
-
+import random
 
 class Persona:
 
     def __init__(self, nombre, edad, sexo, peso, altura):
-        pass
+        self.nombre = nombre
+        self.edad = edad
+        self.sexo = sexo
+        self.peso = peso
+        self.altura = altura
+        self.generar_dni()
 
     def es_mayor_edad(self):
-        pass
+        if self.edad >= 18:
+            return True
 
     # llamarlo desde __init__
     def generar_dni(self):
-        pass
+        aleatorios = [random.randint(0,9) for _ in range(8)]
+        self.dni = int(''.join(str(i) for i in aleatorios))
 
     def print_data(self):
-        pass
+        print("Nombre: {} , Edad: {}, Sexo: {}, Peso: {}, Altura: {}, Dni: {} ".format(self.nombre,self.edad,self.sexo,self.peso,self.altura,self.dni))
+
+assert Persona('Joaquin',23,'M',67,1.67).es_mayor_edad() == True
+Persona('Joaquin',23,'M',67,1.67).print_data()
