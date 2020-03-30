4a4eaa05e4cdd1c87ae409821ba58d12af51e1ad
eche <etxeagus@gmail.com>
2020-03-30 00:08:51 -0300

    nada casi

--- practico_01/ejercicio-14.py
+++ practico_01/ejercicio-14.py
@@ -1,8 +1,25 @@
-#Laberinto True, False, (x,y)
+# Laberinto Si tiene 1 es que puede pasar, si tiene 0 no
+
+# matriz = []
+filas = 3
+columnas = 3
+
+# matriz = [range(columnas) for i in range(filas)]
+matriz = ((1, 0, 0), (1, 0, 0), (1, 1, 1))
+inicio = (0, 0)
+fin = (2, 2)
+
+print(matriz)
+print(matriz[inicio[0]][inicio[1]])
+print(matriz[fin[0]][fin[1]])
+
+
+def CreaLaberinto():
+    pass
+
 
 def encuentraSalida():
-    A = [bool, bool, 0, 0]
-
+    pass
 
 
 def main():
--- practico_03/ejercicio_02.py
+++ practico_03/ejercicio_02.py
@@ -1,13 +1,22 @@
 # Implementar la funcion agregar_persona, que inserte un registro en la tabla Persona
 # y devuelva los datos ingresados el id del nuevo registro.
-
+import mysql.connector
 import datetime
 
 from practico_03.ejercicio_01 import reset_tabla
 
 
 def agregar_persona(nombre, nacimiento, dni, altura):
-    return 0
+    connection = mysql.connector.connect(user="root", password="root", host="localhost", database="soportebd")
+    cursor = connection.cursor()
+    cSQL = "INSERT INTO persona (Nombre, FechaNacimiento, DNI, Altura, IdPersona) VALUES (%s, %s, %s, %s, %s)"
+    persona = (nombre, nacimiento, dni, altura, 0)
+    cursor.execute(cSQL, persona)
+    connection.commit()
+    cselectSQL = "SELECT IdPersona FROM persona WHERE DNI = %s"
+    cursor.execute(cselectSQL, (dni,))
+    result = cursor.fetchone()
+    return result[0]
 
 
 @reset_tabla
@@ -16,6 +25,7 @@ def pruebas():
     id_marcela = agregar_persona('marcela gonzalez', datetime.datetime(1980, 1, 25), 12164492, 195)
     assert id_juan > 0
     assert id_marcela > id_juan
+
 
 if __name__ == '__main__':
     pruebas()
