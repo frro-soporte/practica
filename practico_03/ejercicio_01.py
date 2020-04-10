c9795b1caac254b4e97e526f8b556a6964e3f00e
Martín <martin.ballestero@beew.com.ar>
2020-04-10 15:46:57 -0300

    Uso de funciones de ejericio 01 en ejercicio 02.

--- practico_03/ejercicio_01.py
+++ practico_03/ejercicio_01.py
@@ -5,21 +5,37 @@
 # - DNI: Int()
 # - Altura: Int()
 
+import datetime
+import pandas as pd
+
 # Implementar la funcion borrar_tabla, que borra la tabla creada anteriormente.
 
-
+index = ["IdPersona", "Nombre", "FechaNacimiento", "DNI", "Altura"]
+columns = {}
+df = pd.DataFrame(index=index, columns=columns)
 def crear_tabla():
+    df = pd.DataFrame(index=index, columns=columns)
     pass
 
 
 def borrar_tabla():
+    df.drop(df.index, inplace=True)
     pass
 
+def fulfill(): #Completa columna e imprime
+    df = pd.DataFrame(
+        {"Persona": [1, 'Martin', datetime.date(1992, 10, 13), 39120610, 185]},
+        index = index
+    )
+    print(df)
+    pass
 
 # no modificar
 def reset_tabla(func):
     def func_wrapper():
-        crear_tabla()
-        func()
-        borrar_tabla()
+        crear_tabla() #Inicializa tabla
+        func() #Ejecuta función
+        borrar_tabla() #Borra la tabla
     return func_wrapper
+
+reset_tabla(fulfill())
--- practico_03/ejercicio_02.py
+++ practico_03/ejercicio_02.py
@@ -3,19 +3,20 @@
 
 import datetime
 
-from practico_03.ejercicio_01 import reset_tabla
-
+from ejercicio_01 import reset_tabla, df
 
 def agregar_persona(nombre, nacimiento, dni, altura):
+    df.insert(len(df)+1, nombre, nacimiento, dni, altura)
+    print(df)
     return 0
 
 
-@reset_tabla
+
 def pruebas():
     id_juan = agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180)
     id_marcela = agregar_persona('marcela gonzalez', datetime.datetime(1980, 1, 25), 12164492, 195)
     assert id_juan > 0
     assert id_marcela > id_juan
+    pass
 
-if __name__ == '__main__':
-    pruebas()
+reset_tabla(pruebas)
