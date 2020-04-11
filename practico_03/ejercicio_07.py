84d0dbcead542c3b615e9da1e3ab47624e375748
oberruti <octavio@profitwell.com>
2020-04-11 19:39:59 -0300

    correcciones de codigo identacion y algo de codigo

--- practico_03/ejercicio_07.py
+++ practico_03/ejercicio_07.py
@@ -10,21 +10,21 @@
 import datetime
 import pymysql
 
-from ejercicio_02 import agregar_persona
-from ejercicio_06 import reset_tabla
-from ejercicio_04 import buscar_persona
+from practico_03.ejercicio_02 import agregar_persona
+from practico_03.ejercicio_06 import reset_tabla
+from practico_03.ejercicio_04 import buscar_persona
 
 
 def agregar_peso(id_persona, fecha, peso):
-  if buscar_persona(id_persona):
-        connection=pymysql.connect(
+    if buscar_persona(id_persona):
+        connection = pymysql.connect(
             host='localhost',
             user='root',
             password='lalo123',
             db='Soportetp3')
         cursor = connection.cursor()
-        cSQL = "SELECT FECHA FROM PersonaPeso WHERE IdPersona = %s"
-        cursor.execute(cSQL, id_persona )
+        cSQL = "SELECT Fecha FROM PersonaPeso WHERE IdPersona = %s"
+        cursor.execute(cSQL, id_persona)
         results = cursor.fetchall()
         if results:
             max_date = results[0][0]
@@ -38,20 +38,17 @@ def agregar_peso(id_persona, fecha, peso):
         cursor.execute(cSQL, (id_persona, fecha, peso))
         connection.commit()
         return id_persona
-  else:
-      return False
-
-
+    return False
 
 
 @reset_tabla
 def pruebas():
-    id_juan = agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180)
-    assert agregar_peso(id_juan, datetime.datetime(2018, 5, 26), 80) > 0
+    id_juan = agregar_persona('juan perez', datetime.date(1988, 5, 15), 32165498, 180)
+    assert agregar_peso(id_juan, datetime.date(2018, 5, 26), 80) > 0
     # id incorrecto
-    assert agregar_peso(200, datetime.datetime(1988, 5, 15), 80) == False
+    assert agregar_peso(200, datetime.date(1988, 5, 15), 80) is False
     # registro previo al 2018-05-26
-    assert agregar_peso(id_juan, datetime.datetime(2018, 5, 16), 80) == False
+    assert agregar_peso(id_juan, datetime.date(2018, 5, 16), 80) is False
 
-if __name__ == '__main__':
-    pruebas()
+
+pruebas()
