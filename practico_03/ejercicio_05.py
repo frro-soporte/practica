62b04a972af29aade973dc9284df391743a84f0f
oberruti <octavio@profitwell.com>
2020-04-11 19:28:35 -0300

    correcciones de retorno

--- practico_03/ejercicio_05.py
+++ practico_03/ejercicio_05.py
@@ -4,9 +4,9 @@
 import datetime
 import pymysql
 
-from ejercicio_01 import reset_tabla
-from ejercicio_02 import agregar_persona
-from ejercicio_04 import buscar_persona
+from practico_03.ejercicio_01 import reset_tabla
+from practico_03.ejercicio_02 import agregar_persona
+from practico_03.ejercicio_04 import buscar_persona
 
 
 def actualizar_persona(id_persona, nombre, nacimiento, dni, altura):
@@ -25,17 +25,15 @@ def actualizar_persona(id_persona, nombre, nacimiento, dni, altura):
         cursor.execute(sql1, persona)
         connection.commit()
         return True
-    else:
-        return False
     return False
 
 
 @reset_tabla
 def pruebas():
-    id_juan = agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180)
-    actualizar_persona(id_juan, 'juan carlos perez', datetime.datetime(1988, 4, 16), 32165497, 181)
-    assert buscar_persona(id_juan) == (1, 'juan carlos perez', datetime.datetime(1988, 4, 16), 32165497, 181)
-    assert actualizar_persona(123, 'nadie', datetime.datetime(1988, 4, 16), 12312312, 181) is False
+    id_juan = agregar_persona('juan perez', datetime.date(1988, 5, 15), 32165498, 180)
+    actualizar_persona(id_juan, 'juan carlos perez', datetime.date(1988, 4, 16), 32165497, 181)
+    assert buscar_persona(id_juan) == (1, 'juan carlos perez', datetime.date(1988, 4, 16), 32165497, 181)
+    assert actualizar_persona(123, 'nadie', datetime.date(1988, 4, 16), 12312312, 181) is False
 
-if __name__ == '__main__':
-    pruebas()
+
+pruebas()
