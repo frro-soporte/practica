2d431594a761b5a14e0b7718960567174898fc31
oberruti <octavio@profitwell.com>
2020-04-11 19:19:30 -0300

    Correccion ejercicio 3 return borrar_persona

--- practico_03/ejercicio_03.py
+++ practico_03/ejercicio_03.py
@@ -4,24 +4,21 @@
 import datetime
 import pymysql
 
-from ejercicio_01 import reset_tabla
-from ejercicio_02 import agregar_persona
+from practico_03.ejercicio_01 import reset_tabla
+from practico_03.ejercicio_02 import agregar_persona
 
 
 def borrar_persona(id_persona):
-    connection=pymysql.connect(
+    connection = pymysql.connect(
             host='localhost',
             user='root',
             password='lalo123',
             db='Soportetp3')
     cursor = connection.cursor()
-    sql="DELETE FROM Persona WHERE IdPersona = %s"
-    cursor.execute(sql,id_persona)
+    sql = "DELETE FROM Persona WHERE IdPersona = %s"
+    cursor.execute(sql, id_persona)
     connection.commit()
-    if cursor.rowcount:
-        return True
-    else:
-        return False
+    return False if cursor.rowcount == 0 else True
 
 
 @reset_tabla
@@ -29,5 +26,5 @@ def pruebas():
     assert borrar_persona(agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180))
     assert borrar_persona(12345) is False
 
-if __name__ == '__main__':
-    pruebas()
+
+pruebas()
