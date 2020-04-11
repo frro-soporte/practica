43585e32ad1617952df460421e7319b8822c0601
oberruti <octavio@profitwell.com>
2020-04-11 19:42:00 -0300

    correcciones de codigo identacion

--- practico_03/ejercicio_08.py
+++ practico_03/ejercicio_08.py
@@ -17,39 +17,36 @@
 import datetime
 import pymysql
 
-from ejercicio_02 import agregar_persona
-from ejercicio_06 import reset_tabla
-from ejercicio_07 import agregar_peso
-from ejercicio_04 import buscar_persona
+from practico_03.ejercicio_02 import agregar_persona
+from practico_03.ejercicio_06 import reset_tabla
+from practico_03.ejercicio_07 import agregar_peso
+from practico_03.ejercicio_04 import buscar_persona
 
 
 def listar_pesos(id_persona):
     if buscar_persona(id_persona):
-        connection=pymysql.connect(
+        connection = pymysql.connect(
             host='localhost',
             user='root',
             password='lalo123',
             db='Soportetp3')
         cursor = connection.cursor()
         cSQL = "SELECT Fecha, Peso from PersonaPeso WHERE IdPersona = %s"
-        cursor.execute(cSQL, (id_persona, ))
+        cursor.execute(cSQL, (id_persona,))
         results = cursor.fetchall()
         if results:
             for result in results:
                 print(result)
             return results
-
         return False
-
     return False
-
 
 
 @reset_tabla
 def pruebas():
-    id_juan = agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180)
-    agregar_peso(id_juan, datetime.datetime(2018, 5, 1), 80)
-    agregar_peso(id_juan, datetime.datetime(2018, 6, 1), 85)
+    id_juan = agregar_persona('juan perez', datetime.date(1988, 5, 15), 32165498, 180)
+    agregar_peso(id_juan, datetime.date(2018, 5, 1), 80)
+    agregar_peso(id_juan, datetime.date(2018, 6, 1), 85)
     pesos_juan = listar_pesos(id_juan)
     pesos_esperados = [
         ('2018-05-01', 80),
@@ -57,8 +54,7 @@ def pruebas():
     ]
     assert pesos_juan == pesos_esperados
     # id incorrecto
-    assert listar_pesos(200) == False
+    assert listar_pesos(200) is False
 
 
-if __name__ == '__main__':
-    pruebas()
+pruebas()
