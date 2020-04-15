94ba93c00a92d92e90b7c43045edf2cc2422f701
bonino97 <juanbonino97@gmail.com>
2020-04-15 17:29:47 -0300

    Revert "Merge branch 'Practico-01' of https://github.com/bonino97/frro-soporte-2020-22 into Practico-01"
    This reverts commit 46d36ec281df9dc6a216d9108054dda84b7976b7, reversing
    changes made to bbb2ac2415896b5ab7760a7dd23e31db27492e96.

--- practico_01/ejercicio-02.py
+++ practico_01/ejercicio-02.py
@@ -1,19 +1,9 @@
 # Implementar la función mayor, que reciba tres números y devuelva el mayor de ellos.
 
 
-def maximo(a, b, c):
+def mayor(a, b, c):
     pass
 
-    if b < a > c : 
-        print("El mayor es: ",a)
-        return a
-    if a < b > c : 
-        print("El mayor es: ",b)
-        return b
-    if b < c > a : 
-        print("El mayor es: ",c)
-        return c
-
-# Si no falla es porque esta bien
+# si no falla es porque esta bien
 assert maximo(1,10,5) == 10
 assert maximo(4,9,18) == 18
--- practico_01/ejercicio-10.py
+++ practico_01/ejercicio-10.py
@@ -5,41 +5,8 @@
 # se debe implementar utilizando bucles anidados.
 def superposicion_loop(lista_1, lista_2):
     pass
-    
-    lista_final = []
-    
-    for i in lista_1:
-    
-        if( i not in lista_final) and ( i in lista_2 ):
-    
-            lista_final.append(i)
-            
-    if len(lista_final) == 0: 
-        return False
-    else: 
-        return True
+
 
 # se debe implementar utilizando conjuntos (sets).
 def superposicion_set(lista_1, lista_2):
     pass
-    
-    lista_final = list(set(lista_1) & set(lista_2))
-
-    if len(lista_final) == 0: 
-        return False
-    else: 
-        return True
-
-
-lista1 = [1,2,3,4,5]
-lista2 = [6,7,8]
-
-
-print(superposicion_loop(lista1,lista2))
-print(superposicion_set(lista1,lista2))
-
-lista3 = [1,2,3,4,5]
-lista4 = [4,5,6]
-
-print(superposicion_loop(lista3,lista4))
-print(superposicion_set(lista3,lista4))
