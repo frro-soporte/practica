71f5ee791c8d3ac506ad19a29092760596e6708b
Cristian Gofiar <62672531+cristiangofiar@users.noreply.github.com>
2020-05-02 20:24:57 -0300

    Update ejercicio_07.py

--- practico_03/ejercicio_07.py
+++ practico_03/ejercicio_07.py
@@ -9,9 +9,9 @@
 
 import datetime
 import sqlite3
-from ejercicio_02 import agregar_persona
-from ejercicio_03 import existe_persona
-from ejercicio_06 import reset_tabla
+from practico_03.ejercicio_02 import agregar_persona
+from practico_03.ejercicio_03 import existe_persona
+from practico_03.ejercicio_06 import reset_tabla
 
 
 def agregar_peso(id_persona, fecha, peso):
