a721e09f1085952246b751a0b930084b42d94695
Ramiro <ariasramirox@gmail.com>
2020-06-30 20:44:33 -0300

    Envio mail

---
+++ practico_08/ManejoMail.py
@@ -0,0 +1,29 @@
+import smtplib
+from Mensaje import Mensaje
+import json
+
+
+class ManejoMail(object):
+
+	def __init__(self):
+
+		self.destinatarios = []
+		self.parametros = json.load(open('practico_08/config.json', "r"))
+
+
+	def enviarCorreo(self):		
+
+		message = "<html><h2><b>Reporte de Periodo </b></h2></html>"
+		subject = 'Prueba de correo'
+
+		message = 'Subject: {}\n\n{}'.format(subject, message)
+
+		server = smtplib.SMTP('smtp.gmail.com', 587)
+		server.starttls()
+		server.login('turnosconfesiones@gmail.com', 'sgdpv2020')	
+
+		for destino in self.destinatarios:
+			print ("Enviando correo a ",destino)		
+			server.sendmail('turnosconfesiones@gmail.com', destino, message)	
+
+		server.quit() 
