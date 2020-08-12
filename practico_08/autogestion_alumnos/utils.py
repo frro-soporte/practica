56b601571e665abd3ec2c4a07509b31e0ad94c23
oberruti <octavio@profitwell.com>
2020-08-11 23:38:39 -0300

    . logged() not needed anymore deleted . delete_session_information() not needed anymore deleted

--- practico_08/autogestion_alumnos/utils.py
+++ practico_08/autogestion_alumnos/utils.py
@@ -1,19 +1,5 @@
-from flask import redirect, url_for, session
+from flask import redirect, url_for
 
 
 def go_to(page: str):
     return redirect(url_for(page))
-
-
-def logged():
-    if "userName" in session and "password" in session:
-        return True
-    return False
-
-
-def delete_session_information():
-    if logged():
-        session.pop("userName", None)
-        session.pop("password", None)
-        session.pop("dni", None)
-    return True
