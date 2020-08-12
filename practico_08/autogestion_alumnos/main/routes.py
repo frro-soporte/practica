5327452d982298b198a509e4c67889311038b4ea
oberruti <octavio@profitwell.com>
2020-08-12 01:09:20 -0300

    . main directory added . main routes added

---
+++ practico_08/autogestion_alumnos/main/routes.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, render_template
+from autogestion_alumnos.utils import go_to
+from flask_login import login_required
+# if we need to import app after we need to do 'from flask import current_app'
+
+main = Blueprint('main', __name__)
+
+
+@main.route("/")
+def home():
+    return go_to("users.log_in")
+
+
+@main.route('/<default_page>')
+def redirected_home(default_page):
+    return go_to("users.log_in")
+
+
+@main.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
