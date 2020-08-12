7f021b512e4cbb6fe122eca37df6bf3fdf8dc5e0
oberruti <octavio@profitwell.com>
2020-08-11 23:34:02 -0300

    bcrypt and login_manager added in the init

--- practico_08/autogestion_alumnos/__init__.py
+++ practico_08/autogestion_alumnos/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -10,5 +12,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.permanent_session_lifetime = timedelta(minutes=3)
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'log_in'
 
 from autogestion_alumnos import routes
