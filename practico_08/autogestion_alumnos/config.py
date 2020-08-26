7e05d9d978ca1c6c913df91125dc567240af2396
oberruti <octavio@profitwell.com>
2020-08-25 23:17:28 -0300

    database name changed

--- practico_08/autogestion_alumnos/config.py
+++ practico_08/autogestion_alumnos/config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY = 'frro_soporte'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///login.sqlite3'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///autogestionalumnos.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
