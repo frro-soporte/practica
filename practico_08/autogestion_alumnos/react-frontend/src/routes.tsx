e3f327e87bbabcf2b6da97ddd19c10c63bf2e0ce
oberruti <octavio@profitwell.com>
2020-08-31 21:25:16 -0300

    routes changes

--- practico_08/autogestion_alumnos/react-frontend/src/routes.tsx
+++ practico_08/autogestion_alumnos/react-frontend/src/routes.tsx
@@ -1,67 +1,92 @@
 import React from 'react'
 import { Route, Redirect, Switch } from 'react-router-dom'
-import { login } from './components/login'
+import { Login } from './components/login'
 import { signUp } from './components/signUp'
 import { Layout } from './components/app/layout'
+import { Cookies, withCookies } from 'react-cookie/lib'
+import { isNotNil } from './utils/checks'
 
-export class RoutesApp extends React.Component {
+interface RoutesAppProps {
+    cookies: Cookies
+}
+
+class RoutesApp extends React.Component<RoutesAppProps> {
     render(): JSX.Element {
         return (
             <Switch>
                 <Route exact path="/">
-                    <Redirect to={'app'} />
+                    <Redirect to={'/app'} push={true} />
                 </Route>
-                <Route path="/app" component={App} />
-                <Route path="/acc" component={Acc} />
+                <Route
+                    path="/app"
+                    render={() => <App cookies={this.props.cookies} />}
+                />
+                <Route
+                    path="/acc"
+                    render={() => <Acc cookies={this.props.cookies} />}
+                />
             </Switch>
         )
     }
 }
 
-const isAuthenticated = false
-
-const App = (): JSX.Element => {
+const App = (props: { cookies: Cookies }): JSX.Element => {
+    const accessToken = props.cookies.get('access_token')
+    const isAuthenticated = isNotNil(accessToken)
     if (isAuthenticated) {
         return (
             <Switch>
                 <Route exact path="/app">
-                    <Redirect to={'/app/dashboard'} />
+                    <Redirect to={'/app/dashboard'} push={true} />
                 </Route>
-                <Route path={'/app/dashboard'} component={DashboardLoader} />
+                <Route exact path="/">
+                    <Redirect to={'/app/dashboard'} push={true} />
+                </Route>
+                <Route
+                    path={'/app/dashboard'}
+                    render={() => <DashboardLoader cookies={props.cookies} />}
+                />
             </Switch>
         )
     }
     return (
         <Switch>
-            <Redirect to={'/acc'} />
+            <Redirect to={'/acc'} push={true} />
         </Switch>
     )
 }
 
-const Acc = (): JSX.Element => {
+const Acc = (props: { cookies: Cookies }): JSX.Element => {
+    const accessToken = props.cookies.get('access_token')
+    const isAuthenticated = isNotNil(accessToken)
     if (isAuthenticated) {
         return (
             <Switch>
-                <Redirect to={'/app'} />
+                <Redirect to={'/app'} push={true} />
             </Switch>
         )
     } else
         return (
             <Switch>
                 <Route exact path="/acc">
-                    <Redirect to={'/acc/login'} />
+                    <Redirect to={'/acc/login'} push={true} />
                 </Route>
-                <Route path="/acc/login" component={login} />
+                <Route
+                    path="/acc/login"
+                    render={() => <Login cookies={props.cookies} />}
+                />
                 <Route path="/acc/signup" component={signUp} />
                 <Route path="/acc/password-reset" component={PasswordReset} />
             </Switch>
         )
 }
 
-const DashboardLoader = (): JSX.Element => {
+const DashboardLoader = (props: { cookies: Cookies }): JSX.Element => {
     return <Layout>Log Out</Layout>
 }
 
 const PasswordReset = (): JSX.Element => {
     return <h1>passwordreset</h1>
 }
+
+export default withCookies(RoutesApp)
